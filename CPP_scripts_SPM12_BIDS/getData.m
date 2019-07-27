498beaf39258307f6989807b3c7fb7ff7db0639f
Remi Gau <remi_gau@hotmail.com>
2019-07-27 11:43:01 +0200

    make getData fetch metadata about this task
     - assumes that all subjects and runs have the same metadata for this task
     - also makes the function verbose

--- CPP_scripts_SPM12_BIDS/getData.m
+++ CPP_scripts_SPM12_BIDS/getData.m
@@ -2,12 +2,24 @@ function [group, opt, BIDS] = getData(opt)
 % The following structure will be the base that the scripts will use to run
 % the preprocessing pipeline according to the BIDS Structure
 
+fprintf(1,'FOR TASK: %s\n', opt.taskName)
+
 % The directory where the derivatives are located
 derivativesDir = opt.derivativesDir;
 
 BIDS = spm_BIDS(derivativesDir);
 
+% get IDs of all subjects
 subjects = spm_BIDS(BIDS, 'subjects');
+
+% get metadata for bold runs for that task 
+% we take those from the first run of the first subject assuming it can
+% apply to all others.
+metadata = spm_BIDS(BIDS, 'metadata', ...
+    'task', opt.taskName, ...
+    'sub', subjects{1}, ...
+    'type', 'bold');
+opt.metadata = metadata{1};
 
 % Add the different groups in your experiment
 %% GROUP 1
