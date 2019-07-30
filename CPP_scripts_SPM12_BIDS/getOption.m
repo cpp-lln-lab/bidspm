b0004f24c3e9d6244ce1bb8e2ab8459748bd27d0
Remi Gau <remi_gau@hotmail.com>
2019-07-30 11:37:42 +0200

    Update help section and TO DO list in getOption

--- CPP_scripts_SPM12_BIDS/getOption.m
+++ CPP_scripts_SPM12_BIDS/getOption.m
@@ -1,7 +1,18 @@
 function opt = getOption()
+% returns a structure that contains the options chosen by the user to run
+% slice timing correction, pre-processing, FFX, RFX.
 
 % TO DO
-% implement a way to only select some subjects from each group
+% - implement a way to only select some subjects from each group < -- Did we
+% TEST this ????
+% - create a function to check the options and set some defaults in case
+% they are missing.
+% - implement a way to give some specific names to each subject otherwise
+% it will always start from '01' even if the user wants to skip X subjects
+% that have alreay been processed
+% - create a function that copies the raw data into a derivatives/SPM12-CPP
+% directory (also make sure that the raw data is in read only mode)
+% - make the name of the opt.mat different for each task ????
 
 if nargin<1
     opt = [];
@@ -9,6 +20,7 @@ end
 
 % group of subjects to analyze
 opt.groups = {'con'};    % {'blnd', 'ctrl'};
+% suject to run in each group
 opt.subjects = {[1 2]};  % {[1:2], [1:2]};
 
 % task to analyze
