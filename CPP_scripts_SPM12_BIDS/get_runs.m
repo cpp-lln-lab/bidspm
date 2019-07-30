fd820e5d10d7e79c1db2516d139a96d68a7c69a8
Remi Gau <remi_gau@hotmail.com>
2019-07-30 11:37:03 +0200

    Add help section to get_session/run/filemame

--- CPP_scripts_SPM12_BIDS/get_runs.m
+++ CPP_scripts_SPM12_BIDS/get_runs.m
@@ -1,6 +1,21 @@
-function [runs, numRuns] = get_runs(BIDS, subNumber, session, opt)
+function [runs, numRuns] = get_runs(BIDS, subID, session, opt)
+% for a given BIDS data set, subject identity, session and options this returns the
+% name of the runs and their number
+%
+% INPUTS
+% BIDS - variable returned by spm_BIDS when exploring a BIDS data set
+% subID - ID of the subject ; in BIDS lingo that means that for a file name
+% sub-02_task-foo_bold.nii the subID will be the string '02'
+% session - ID of the session of interes ; in BIDS lingo that means that for a file name
+% sub-02_ses-pretest_task-foo_bold.nii the sesssion will be the string 'pretest'
+% opt - options structure defined by the getOption function. Mostly used to find the
+% task name.
+
+% TO CHECK
+% what happens if several subID/sessions are given as input (as a cell of strings)?
+
 runs = spm_BIDS(BIDS, 'runs', ...
-    'sub', subNumber, ...
+    'sub', subID, ...
     'task', opt.taskName, ...
     'ses', session, ...
     'type', 'bold');
