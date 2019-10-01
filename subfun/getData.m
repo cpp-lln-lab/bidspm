52568d1da8b94885e757b0fd094691cc2ccc922f
Remi Gau <remi_gau@hotmail.com>
2019-10-01 16:39:29 +0200

    getData sets some default options
     - in case they have not been specified by the user

--- subfun/getData.m
+++ subfun/getData.m
@@ -2,6 +2,8 @@ function [group, opt, BIDS] = getData(opt)
 % The following structure will be the base that the function will use to run
 % the preprocessing pipeline according to the BIDS Structure
 
+opt = checkOptions(opt);
+
 fprintf(1,'FOR TASK: %s\n', opt.taskName)
 
 % The directory where the derivatives are located
@@ -28,8 +30,8 @@ end
 
 %% Add the different groups in the experiment
 for iGroup = 1:numel(opt.groups) % for each group
-    group(iGroup).name = opt.groups{iGroup}; %#ok<*AGROW>                            % NAME OF THE GROUP 
-
+    group(iGroup).name = opt.groups{iGroup}; %#ok<*AGROW>                            % NAME OF THE GROUP
+    
     % we figure out which subjects to take if not specified take all subjects
     if isfield(opt,'subjects') && ~isempty(opt.subjects{iGroup})
         idx = opt.subjects{iGroup};
@@ -43,13 +45,62 @@ for iGroup = 1:numel(opt.groups) % for each group
             idx = find(~cellfun('isempty', idx));
         end
     end
-
+    
     group(iGroup).subNumber = subjects(idx);                            % SUBJECT ID .. con01 , con02 , etc.
     group(iGroup).numSub = length(group(iGroup).subNumber) ;            % Number of subjects in the group
-
+    
     fprintf(1,'WILL WORK ON SUBJECTS\n')
     disp(group(iGroup).subNumber)
 end
 
 
+end
+
+
+function opt = checkOptions(opt)
+options = getDefaultOption();
+
+% update options with user input
+% --------------------------------
+I = intersect(fieldnames(options), fieldnames(opt));
+
+for f = 1:length(I)
+    options = setfield( options,I{f}, getfield(opt,I{f}));
+end
+
+opt = options;
+
+end
+
+
+function options = getDefaultOption()
+% group of subjects to analyze
+options.groups = {''};
+% suject to run in each group
+options.subjects = {[]};  % {[1:2], [1:2]};
+
+% task to analyze
+options.taskName = '';
+
+% The directory where the derivatives are located
+options.derivativesDir = '';
+
+% Specify the number of dummies that you want to be removed.
+options.numDummies = 0;
+options.dummyPrefix = 'dr_';
+
+% Options for slice time correction
+options.STC_referenceSlice = []; % reference slice: middle acquired slice
+options.sliceOrder = []; % To be used if SPM can't extract slice info
+
+% Options for normalize
+% Voxel dimensions for resampling at normalization of functional data or leave empty [ ].
+options.funcVoxelDims = [];
+
+% Suffix output directory for the saved jobs
+options.JOBS_dir = '';
+
+% specify the model file that contains the contrasts to compute
+options.contrastList = {};
+options.model.file = '';
 end
