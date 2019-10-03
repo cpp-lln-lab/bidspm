dade2aba1badc044a16214d2a27b9654ae98da96
Moahmed Rezk <phtawfik@gmail.com>
2019-10-03 17:30:18 +0200

    update pmCon change model.Steps(iStep) to model.Steps{iStep}

--- subfun/pmCon.m
+++ subfun/pmCon.m
@@ -26,69 +26,69 @@ con_counter = 0;
 
 % for the task of interest
 if strcmp(model.Input.task, taskName)
-    
+
     % check all the steps specified in the model
     for iStep = 1:length(model.Steps)
-        
+
         % only compute contrasts at the subject level
-        if strcmp(model.Steps(iStep).Level, 'subject')
-            
+        if strcmp(model.Steps{iStep}.Level, 'subject')
+
             % specify all the contrasts
-            
+
             % first the contrasts to compute automatically against baseline
-            for iCon = 1:length(model.Steps(iStep).AutoContrasts)
-                
+            for iCon = 1:length(model.Steps{iStep}.AutoContrasts)
+
                 con_counter = con_counter + 1;
-                
+
                 C = zeros(1,size(SPM.xX.X,2));
-                
+
                 % get condition name
-                cdt_name = model.Steps(iStep).AutoContrasts{iCon};
+                cdt_name = model.Steps{iStep}.AutoContrasts{iCon};
                 cdt_name = strrep(cdt_name, 'trial_type.', '');
-                
+
                 % get regressors index corresponding to the HRF of that condition
                 regIdx = strfind(SPM.xX.name', [' ' cdt_name '*bf(1)']);
                 regIdx = ~cellfun('isempty', regIdx);  %#ok<*STRCL1>
-                
+
                 % give them a value of 1
                 C(end,regIdx) = 1;
-                
+
                 % stores the specification
                 contrasts(con_counter).C = C;
                 contrasts(con_counter).name =  cdt_name;
-                
+
             end
-            
+
             % then the contrasts that involve contrasting conditions
             % amongst themselves or soemthing inferior to baseline
-            for iCon = 1:length(model.Steps(iStep).Contrasts)
-                
+            for iCon = 1:length(model.Steps{iStep}.Contrasts)
+
                 con_counter = con_counter + 1;
-                
+
                 C = zeros(1,size(SPM.xX.X,2));
-                
-                for iCdt = 1:length(model.Steps(iStep).Contrasts(iCon).ConditionList)
-                    
+
+                for iCdt = 1:length(model.Steps{iStep}.Contrasts(iCon).ConditionList)
+
                     % get condition name
-                    cdt_name = model.Steps(iStep).Contrasts(iCon).ConditionList{iCdt};
+                    cdt_name = model.Steps{iStep}.Contrasts(iCon).ConditionList{iCdt};
                     cdt_name = strrep(cdt_name, 'trial_type.', '');
-                    
+
                     % get regressors index corresponding to the HRF of that condition
                     regIdx = strfind(SPM.xX.name', [' ' cdt_name '*bf(1)']);
                     regIdx = ~cellfun('isempty', regIdx);
-                    
+
                     % give them a value of 1
-                    C(end,regIdx) = model.Steps(iStep).Contrasts(iCon).weights(iCdt);
-                    
+                    C(end,regIdx) = model.Steps{iStep}.Contrasts(iCon).weights(iCdt);
+
                 end
-                
+
                 % stores the specification
                 contrasts(con_counter).C = C;
                 contrasts(con_counter).name =  ...
-                    model.Steps(iStep).Contrasts(iCon).Name;
-                
+                    model.Steps{iStep}.Contrasts(iCon).Name;
+
             end
-            
+
         end
     end
 end
@@ -98,38 +98,38 @@ end
 
 %%
 % C = [C ;zeros(1,size(SPM.xX.X,2))]; % add 1 lign to C (more flexible than adding a fixed whole bunch at once)
-% 
+%
 % for iContrast=1:size(SPM.xX.X,2)
 %     if findstr(SPM.xX.name{iContrast},'VisMot*bf(1)')
 %         C(end,iContrast) = 1;
 %     end
 % end
-% 
+%
 % for iContrast=1:size(SPM.xX.X,2)
 %     if findstr(SPM.xX.name{iContrast},'VisStat*bf(1)')
 %         C(end,iContrast) = -1;
 %     end
 % end
-% 
+%
 % line_counter = line_counter + 1;
 % contrasts(line_counter).C = C(end,:);
 % contrasts(line_counter).name =  'VisMot - VisStat';
-% 
+%
 % %%
 % C = [C ;zeros(1,size(SPM.xX.X,2))]; % add 1 lign to C (more flexible than adding a fixed whole bunch at once)
-% 
+%
 % for iContrast=1:size(SPM.xX.X,2)
 %     if findstr(SPM.xX.name{iContrast},'VisStat*bf(1)')
 %         C(end,iContrast) = 1;
 %     end
 % end
-% 
+%
 % for iContrast=1:size(SPM.xX.X,2)
 %     if findstr(SPM.xX.name{iContrast},'VisMot*bf(1)')
 %         C(end,iContrast) = -1;
 %     end
 % end
-% 
+%
 % line_counter = line_counter + 1;
 % contrasts(line_counter).C = C(end,:);
 % contrasts(line_counter).name =  'VisStat - VisMot';
