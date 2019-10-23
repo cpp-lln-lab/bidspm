775b3833273c779c1b99b1da7c3d846b76d3bd7c
Remi Gau <remi_gau@hotmail.com>
2019-10-23 08:52:48 +0200

    debug pmCon

--- subfun/pmCon.m
+++ subfun/pmCon.m
@@ -26,10 +26,10 @@ con_counter = 0;
 
 % for the task of interest
 if strcmp(model.Input.task, taskName)
-
+    
     % check all the steps specified in the model
     for iStep = 1:length(model.Steps)
-
+        
         switch model.Steps{iStep}.Level
             
             %% compute contrasts at the subject level
@@ -37,79 +37,91 @@ if strcmp(model.Input.task, taskName)
                 
                 % specify all the contrasts
                 
-                % first the contrasts to compute automatically against baseline
-                for iCon = 1:length(model.Steps{iStep}.AutoContrasts)
+                if isfield(model.Steps{iStep}, 'AutoContrasts')
                     
-                    con_counter = con_counter + 1;
-                    
-                    C = zeros(1,size(SPM.xX.X,2));
-                    
-                    % get regressors index corresponding to the HRF of that condition
-                    regIdx = getRegIdx(model, iStep, iCon, SPM);
-                    
-                    % give them a value of 1
-                    C(end,regIdx) = 1;
-                    
-                    % stores the specification
-                    contrasts(con_counter).C = C;
-                    contrasts(con_counter).name =  cdt_name;
-                    
-                end
-                
-                % then the contrasts that involve contrasting conditions
-                % amongst themselves or something inferior to baseline
-                for iCon = 1:length(model.Steps{iStep}.Contrasts)
-                    
-                    con_counter = con_counter + 1;
-                    
-                    C = zeros(1,size(SPM.xX.X,2));
-                    
-                    for iCdt = 1:length(model.Steps{iStep}.Contrasts(iCon).ConditionList)
+                    % first the contrasts to compute automatically against baseline
+                    for iCon = 1:length(model.Steps{iStep}.AutoContrasts)
+                        
+                        con_counter = con_counter + 1;
+                        
+                        C = zeros(1,size(SPM.xX.X,2));
                         
                         % get regressors index corresponding to the HRF of that condition
-                        regIdx = getRegIdx(model, iStep, iCon, SPM);
+                        [cdt_name, regIdx] = getRegIdx(model, iStep, iCon, SPM);
                         
                         % give them a value of 1
-                        C(end,regIdx) = model.Steps{iStep}.Contrasts(iCon).weights(iCdt);
+                        C(end,regIdx) = 1;
+                        
+                        % stores the specification
+                        contrasts(con_counter).C = C;
+                        contrasts(con_counter).name = cdt_name;
                         
                     end
                     
-                    % stores the specification
-                    contrasts(con_counter).C = C;
-                    contrasts(con_counter).name =  ...
-                        model.Steps{iStep}.Contrasts(iCon).Name;
-                    
                 end
                 
                 
-            %% compute contrasts at the run level
+                if isfield(model.Steps{iStep}, 'Contrasts')
+                    
+                    % then the contrasts that involve contrasting conditions
+                    % amongst themselves or something inferior to baseline
+                    for iCon = 1:length(model.Steps{iStep}.Contrasts)
+                        
+                        con_counter = con_counter + 1;
+                        
+                        C = zeros(1,size(SPM.xX.X,2));
+                        
+                        for iCdt = 1:length(model.Steps{iStep}.Contrasts(iCon).ConditionList)
+                            
+                            % get regressors index corresponding to the HRF of that condition
+                            [~, regIdx] = getRegIdx(model, iStep, iCon, SPM);
+                            
+                            % give them a value of 1
+                            C(end,regIdx) = model.Steps{iStep}.Contrasts(iCon).weights(iCdt);
+                            
+                        end
+                        
+                        % stores the specification
+                        contrasts(con_counter).C = C;
+                        contrasts(con_counter).name =  ...
+                            model.Steps{iStep}.Contrasts(iCon).Name;
+                        
+                    end
+                    
+                end
+                
+                %% compute contrasts at the run level
             case 'run'
                 
                 % specify all the contrasts
                 
-                % first the contrasts to compute automatically against baseline
-                for iCon = 1:length(model.Steps{iStep}.AutoContrasts)
+                if isfield(model.Steps{iStep}, 'AutoContrasts')
                     
-                    % get regressors index corresponding to the HRF of that condition
-                    regIdx = getRegIdx(model, iStep, iCon, SPM);
-                    
-                    regIdx = find(regIdx);
-                    
-                    % For each event of each condition, create a seperate
-                    % contrast
-                    for iReg=1:length(regIdx)
+                    % first the contrasts to compute automatically against baseline
+                    for iCon = 1:length(model.Steps{iStep}.AutoContrasts)
                         
-                        C = zeros(1,size(SPM.xX.X,2));
+                        % get regressors index corresponding to the HRF of that condition
+                        [cdt_name, regIdx] = getRegIdx(model, iStep, iCon, SPM);
                         
-                        % add a new line for a new contrast
-                        con_counter = con_counter + 1;
-                        C(end,regIdx(iReg)) = 1 ;   % give each event a value of 1
+                        regIdx = find(regIdx);
                         
-                        % stores the specification
-                        contrasts(con_counter).C = C(con_counter,:);
-                        contrasts(con_counter).name =  [cdt_name, '_', num2str(iReg)];
-                        
+                        % For each event of each condition, create a seperate
+                        % contrast
+                        for iReg=1:length(regIdx)
+                            
+                            C = zeros(1,size(SPM.xX.X,2));
+                            
+                            % add a new line for a new contrast
+                            con_counter = con_counter + 1;
+                            C(end,regIdx(iReg)) = 1 ;   % give each event a value of 1
+                            
+                            % stores the specification
+                            contrasts(con_counter).C = C;
+                            contrasts(con_counter).name =  [cdt_name, '_', num2str(iReg)];
+                            
+                        end
                     end
+                    
                 end
         end
         
@@ -122,7 +134,7 @@ end
 
 
 
-function  regIdx = getRegIdx(model, iStep, iCon, SPM)
+function  [cdt_name, regIdx] = getRegIdx(model, iStep, iCon, SPM)
 % get regressors index corresponding to the HRF of of a condition
 
 % get condition name
