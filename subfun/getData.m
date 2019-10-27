a6956b349f009af7889ba38709066aec2cf9cb8c
Remi Gau <remi_gau@hotmail.com>
2019-10-27 09:32:32 +0100

    fix indexing issue in getData

--- subfun/getData.m
+++ subfun/getData.m
@@ -1,6 +1,26 @@
 function [group, opt, BIDS] = getData(opt)
-% The following structure will be the base that the function will use to run
-% the preprocessing pipeline according to the BIDS Structure
+% getData checks that all the options specified by the user in getOptions
+% and fills the blank for any that might have been missed out.
+% It then reads the specified BIDS data set and gets the groups and
+% subjects to analyze. This can be specified in the opt structure in
+% different ways:
+% Set the group of subjects to analyze.
+% opt.groups = {'control', 'blind'};
+%
+% If there are no groups (i.e subjects names are of the form `sub-01` for
+% example) or if you want to run all subjects of all groups then use:
+% opt.groups = {''};
+% opt.subjects = {[]};
+%
+% If you have 2 groups (`cont` and `cat` for example) the following will
+% run cont01, cont02, cat03, cat04.
+% opt.groups = {'cont', 'cat'};
+% opt.subjects = {[1 2], [3 4]};
+%
+% If you have more than 2 groups but want to only run the subjects of 2
+% groups then you can use.
+% opt.groups = {'cont', 'cat'};
+% opt.subjects = {[], []};
 
 opt = checkOptions(opt);
 
@@ -22,6 +42,7 @@ metadata = spm_BIDS(BIDS, 'metadata', ...
     'task', opt.taskName, ...
     'sub', subjects{1}, ...
     'type', 'bold');
+
 if iscell(metadata)
     opt.metadata = metadata{1};
 else
@@ -31,37 +52,58 @@ end
 %% Add the different groups in the experiment
 for iGroup = 1:numel(opt.groups) % for each group
     
-     % Name of the group
-    group(iGroup).name = opt.groups{iGroup}; %#ok<*AGROW>                           
-
-    % we figure out which subjects to take if not specified take all subjects
-    if isfield(opt,'subjects') && ~isempty(opt.subjects{iGroup})
+    clear idx
+    
+    % Name of the group
+    group(iGroup).name = opt.groups{iGroup}; %#ok<*AGROW>
+    
+    % if no group or subject was specified we take all of them
+    if numel(opt.groups)==1 && strcmp(group(iGroup).name, '') && isempty(opt.subjects{iGroup})
+        
+        group(iGroup).subNumber = subjects;
+        
+    % if subject ID were directly specified by users we take those
+    elseif strcmp(group(iGroup).name, '') && iscell(opt.subjects)
+        
+        group(iGroup).subNumber = opt.subjects;
+        
+    % if group was specified we figure out which subjects to take
+    elseif ~isempty(opt.subjects{iGroup})
         
         idx = opt.subjects{iGroup};
+
+    % else we take all subjects of that group
+    elseif isempty(opt.subjects{iGroup})
         
-        % Number of subjects in the group
-        group(iGroup).numSub = length(idx) ; 
-        
-        % find the subjects that belong to that group
-        groupSubIdx = ~cellfun('isempty', strfind(subjects,group(iGroup).name)); % compare the subjects with the group name 
-        groupSubs = subjects(groupSubIdx) ;                                  % Get the index of the subjects corresponding to this group
-        group(iGroup).subNumber = groupSubs(idx);                            % SUBJECT ID .. con01 , con02 , etc.
+        % count how many subjects in that group
+        idx = sum(~cellfun(@isempty, strfind(subjects, group(iGroup).name)));
+        idx = 1:idx;
         
     else
-        
-        % in case no group was specified (e.g. sub-01) we need a way to still
-        % get the subjects ID
-        if strcmp(group(iGroup).name, '')
-            idx = 1:size(subjects,2);
-        else
-            idx = strfind(subjects, group(iGroup).name);
-            idx = find(~cellfun('isempty', idx));
-        end      
-        group(iGroup).subNumber = subjects(idx);                            % SUBJECT ID .. con01 , con02 , etc.
-        group(iGroup).numSub = length(group(iGroup).subNumber) ;            % Number of subjects in the group
-        
+        error('Not sure what to do.')
+
     end
     
+    % if only indices were specified we get the subject from that group with that
+    if exist('idx', 'var')
+        pattern = [group(iGroup).name '%0' num2str(opt.zeropad) '.0f_'];
+        temp = split(sprintf(pattern, idx) ,'_');
+        group(iGroup).subNumber = temp(1:end-1)';
+    end
+    
+    
+    % check that all the subjects asked for exist
+    if ~all(ismember(group(iGroup).subNumber, subjects))
+        fprintf('subjects specified\n')
+        disp(group(iGroup).subNumber)
+        fprintf('subjects present\n')
+        disp(subjects)
+        error('Some of the subjects specified do not exist in this data set. This can be due to wrong zero padding: see opt.zeropad in getOptions')
+    end
+    
+    
+    % Number of subjects in the group
+    group(iGroup).numSub = length(group(iGroup).subNumber) ;
     
     fprintf(1,'WILL WORK ON SUBJECTS\n')
     disp(group(iGroup).subNumber)
@@ -80,12 +122,12 @@ options = getDefaultOption();
 I = intersect(fieldnames(options), fieldnames(opt));
 
 for f = 1:length(I)
-    options = setfield( options,I{f}, getfield(opt,I{f}));
+    options = setfield( options,I{f}, getfield(opt,I{f}) ); %#ok<GFLD,SFLD>
 end
 
-if strcmp (options.groups{1}, '') && ~isempty(options.subjects{1})
-    warning('you want to analyze all groups but still mentioned a subject index : this is not supported at the moment. We will take all the subjects from all groups.')
-    options.subjects = {[]}; 
+if ~all(cellfun(@isstr, options.groups))
+    disp(options.groups)
+    error('All group names should be string.')
 end
 
 if ~isempty (options.STC_referenceSlice) && length(options.STC_referenceSlice)>1
@@ -107,7 +149,8 @@ function options = getDefaultOption()
 % group of subjects to analyze
 options.groups = {''};
 % suject to run in each group
-options.subjects = {[]}; 
+options.subjects = {[]};
+options.zeropad = 2;
 
 % task to analyze
 options.taskName = '';
