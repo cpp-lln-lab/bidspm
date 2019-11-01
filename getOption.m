26af47329e3c1521a083f1cfd6b1b3393c62b61c
Remi Gau <remi_gau@hotmail.com>
2019-11-01 14:08:21 +0100

    update getOptions

--- getOption.m
+++ getOption.m
@@ -9,19 +9,29 @@ end
 % group of subjects to analyze
 opt.groups = {''}; % {'blnd', 'ctrl'};
 % suject to run in each group
-opt.subjects = {1:3};  % {[1:2], [1:2]};
+% opt.subjects = {[4:6]};  % {[1:2], [1:2]};
+opt.subjects = {[]};  % {[1:2], [1:2]};
 
 
 % task to analyze
-opt.taskName = 'motionDecoding';
+opt.taskName = 'MotionDecoding';
+
 % opt.taskName = 'visMotion';
+
+% opt.taskName = 'balloonanalogrisktask';
+
 
 
 % The directory where the derivatives are located
+
 % opt.dataDir = '/Users/mohamed/Desktop/MotionWorkshop/raw';
 % opt.dataDir = '/Users/mohamed/Desktop/Data/raw';
+
 % opt.dataDir = '/home/remi/BIDS/visMotion/raw';
-opt.dataDir = '/home/remi/BIDS/motionDecoding/raw';
+opt.dataDir = '/home/remi/BIDS/MotionDecoding/raw';
+
+% opt.dataDir = '/home/remi/BIDS/ds001/rawdata';
+
 
 % Options for slice time correction
 % If left unspecified the slice timing will be done using the mid-volume acquisition
@@ -42,11 +52,18 @@ opt.funcVoxelDims = [];
 opt.JOBS_dir = fullfile(opt.dataDir, '..', 'derivatives', 'SPM12_CPPL', 'JOBS', opt.taskName);
 
 % specify the model file that contains the contrasts to compute
+
 % opt.model.univariate.file = '/Users/mohamed/Documents/GitHub/BIDS_fMRI_scripts/model-motionDecodingUnivariate_smdl.json';
 % opt.model.multivariate.file = '/Users/mohamed/Documents/GitHub/BIDS_fMRI_scripts/model-motionDecodingMultivariate_smdl.json';
+
 % opt.model.univariate.file = '/home/remi/github/CPP_BIDS_SPM_pipeline/model-visMotionLoc_smdl.json';
+
 opt.model.univariate.file = '/home/remi/github/CPP_BIDS_SPM_pipeline/model-motionDecodingUnivariate_smdl.json';
 opt.model.multivariate.file = '/home/remi/github/CPP_BIDS_SPM_pipeline/model-motionDecodingMultivariate_smdl.json';
+
+% opt.model.univariate.file = '/home/remi/github/CPP_BIDS_SPM_pipeline/model-balloonanalogriskUnivariate_smdl.json';
+% opt.model.multivariate.file = '/home/remi/github/CPP_BIDS_SPM_pipeline/model-balloonanalogriskMultivariate_smdl.json';
+
 
 % Save the opt variable as a mat file to load directly in the preprocessing
 % scripts
