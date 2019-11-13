85a1644ea68799eb386ab30efd4706a9eb0eee3c
Remi Gau <remi_gau@hotmail.com>
2019-11-13 19:51:37 +0100

    add model files to test and fix json files

--- test/test_modelFiles.m
+++ test/test_modelFiles.m
@@ -33,5 +33,19 @@ model = spm_jsonread(file);
 
 model.Steps{1}
 
+%%
+file = fullfile(fileparts(mfilename), '..', 'model', 'model-balloonanalogriskUnivariate_smdl.json');
+
+model = spm_jsonread(file);
+
+model.Steps{1}
+
+%%
+file = fullfile(fileparts(mfilename), '..', 'model', 'model-balloonanalogriskMultivariate_smdl.json');
+
+model = spm_jsonread(file);
+
+model.Steps{1}
+
 
 end
