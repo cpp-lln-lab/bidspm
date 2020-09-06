a1502cb200246acd5a44bed7e318a147f0c08638
Remi Gau <remi_gau@hotmail.com>
2020-09-06 20:33:10 +0200

    add test for getBoldFilename

--- src/getBoldFilename.m
+++ src/getBoldFilename.m
@@ -1,9 +1,10 @@
 function [fileName, subFuncDataDir] = getBoldFilename(varargin)
-
-    [BIDS, subID, sessions, runs, opt] = deal(varargin{:});
+    % [fileName, subFuncDataDir] = getBoldFilename(BIDS, opt, subID, sessionID, runID)
+    
+    [BIDS, subID, sessionID, runID, opt] = deal(varargin{:});
 
     % get the filename for this bold run for this task
-    fileName =  getInfo(BIDS, subID, opt, 'Filename', sessions, runs, 'bold');
+    fileName =  getInfo(BIDS, subID, opt, 'Filename', sessionID, runID, 'bold');
 
     % get fullpath of the file
     fileName = fileName{1};
---
+++ tests/test_getBoldFilename.m
@@ -0,0 +1,39 @@
+function test_suite = test_getBoldFilename %#ok<*STOUT>
+    try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+        test_functions = localfunctions(); %#ok<*NASGU>
+    catch % no problem; early Matlab versions can use initTestSuite fine
+    end
+    initTestSuite;
+end
+
+function test_getBoldFilenameBasic()
+
+    subID = '01';
+    funcFWHM = 6;
+    iSes = 1;
+    iRun = 1;
+
+    opt.taskName = 'vislocalizer';
+    opt.dataDir = fullfile(fileparts(mfilename('fullpath')), 'dummyData', 'derivatives');
+    opt.groups = {''};
+    opt.subjects = {'01'};
+
+    [~, opt, BIDS] = getData(opt);
+    
+    sessions = getInfo(BIDS, subID, opt, 'Sessions');
+
+    runs = getInfo(BIDS, subID, opt, 'Runs', sessions{iSes});
+
+    [fileName, subFuncDataDir] = getBoldFilename( ...
+        BIDS, ...
+        subID, sessions{iSes}, runs{iRun}, opt);
+
+    expectedFileName = 'sub-01_ses-01_task-vislocalizer_bold.nii';
+    
+    expectedSubFuncDataDir = fullfile(fileparts(mfilename('fullpath')), ...
+        'dummyData', 'derivatives', 'SPM12_CPPL', 'sub-01', 'ses-01', 'func');
+
+    assertEqual(expectedSubFuncDataDir, subFuncDataDir);
+    assertEqual(expectedFileName, fileName);
+
+end
