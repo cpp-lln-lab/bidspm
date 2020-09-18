20cb472df73e087c37fe8fa6d5c08b83df13627c
Remi Gau <remi_gau@hotmail.com>
2020-09-18 16:55:25 +0200

    refactor manual coregistration
    refactor mancoreg
    
    
    refactor mancoreg
    
    
    refacor mancoreg

--- src/mancoreg/mancoreg.m
+++ src/mancoreg/mancoreg.m
@@ -1,4 +1,10 @@
+% (C) Copyright 2004-2009 JH
+% (C) Copyright 2009_2012 DSS
+% (C) Copyright 2012_2019 Remi Gau
+% (C) Copyright 2019 CPP BIDS SPM-pipeline developpers
+
 function mancoreg(targetimage, sourceimage)
+    % mancoreg(targetimage, sourceimage)
     %
     % Manual coregistration tool
     %
@@ -20,7 +26,7 @@ function mancoreg(targetimage, sourceimage)
     %
     % The code is loosely based on spm_image.m and spm_orthoviews.m
     % It requires the m-file with the callback functions for the user
-    % controls (mancoreg_callbacks.m).
+    % controls (mancoregCallbacks.m).
     % ----------------------------------------------------------------------------
     % JH 10.01.2004
     % modified DSS 10/02/2009
@@ -32,22 +38,38 @@ function mancoreg(targetimage, sourceimage)
     % two radio buttons for toggle on/off (12.1.2004, JH)
     %
     % Version 1.0.2
-    % Added:    Plot of transformation matrix, values are shown next to sliders
-    %           and "reset transformation" button (12.1.2004, JH)
+    % Added: Plot of transformation matrix, values are shown next to sliders
+    % and "reset transformation" button (12.1.2004, JH)
     %
     % Version 1.0.3
-    % Added:    Made compatible with SPM5 and corrected "Yawn" (altho I like it)
+    % Added: Made compatible with SPM5 and corrected "Yawn" (altho I like it)
 
     global st mancoregvar
 
-    %% Make sure we have both image filenames and map images
-    % ----------------------------------------------------------------------------
+    opt.smallFontSize = 8;
+    opt.largeFontSize = 10;
 
-    if ~exist('targetimage')
+    opt.smallFontBoxHeight = 15;
+    opt.largeFontBoxHeight = 19;
+
+    opt.smallFontBoxWidth = 40;
+    opt.largeFontBoxWidth = 60;
+
+    opt.translationSliderMin = -500;
+    opt.translationSliderMax = 500;
+
+    opt.rotationSliderMin = -pi;
+    opt.rotationSliderMax = pi;
+
+    opt.sliderStep = [0.01 0.01];
+
+    %% Make sure we have both image filenames and map images
+
+    if ~exist('targetimage', 'var')
         targetimage = spm_select(1, 'image', 'Please select target image');
     end
 
-    if ~exist('sourceimage')
+    if ~exist('sourceimage', 'var')
         sourceimage = spm_select(1, 'image', 'Please select source image');
     end
 
@@ -55,7 +77,6 @@ function mancoreg(targetimage, sourceimage)
     sourcevol = spm_vol(sourceimage);
 
     %% Init graphics window
-    % ----------------------------------------------------------------------------
 
     fg = spm_figure('Findwin', 'Graphics');
     if isempty(fg)
@@ -66,7 +87,7 @@ function mancoreg(targetimage, sourceimage)
     else
         spm_figure('Clear', 'Graphics');
     end
-    WS = spm('WinScale');
+    opt.windowScale = spm('WinScale');
 
     htargetstim = spm_orthviews('Image', targetvol, [0.1 0.55 0.45 0.48]);
     spm_orthviews('space');
@@ -81,97 +102,278 @@ function mancoreg(targetimage, sourceimage)
     mancoregvar.sourceimage = st.vols{2};
 
     %% Define and initialise all user controls
-    % ----------------------------------------------------------------------------
 
-    % 1.a Titles and boxes
+    initTitleAndBoxes(opt, fg, targetimage, sourceimage);
+    mancoregvar = initTransMat(mancoregvar, opt, fg);
+    mancoregvar = initRotationSlider(mancoregvar, opt, fg);
+    mancoregvar = initTranslationSlider(mancoregvar, opt, fg);
 
-    uicontrol(fg, 'style', 'text', 'string', 'Manual coregistration tool', 'position', [200 825 200 30] .* WS, 'Fontsize', 16, 'backgroundcolor', [1 1 1]);
+    % Source/target display toggle
 
-    uicontrol(fg, 'style', 'frame', 'position', [360 750 240 60] .* WS);
-    uicontrol(fg, 'style', 'frame', 'position', [360 40 240 410] .* WS);
+    mancoregvar.htoggle_off = uicontrol(fg, ...
+        'style', 'radiobutton', ...
+        'position', ...
+        [470 100 opt.smallFontBoxWidth * 2 opt.largeFontBoxHeight] .* opt.windowScale, ...
+        'Value', 1, ...
+        'Callback', 'mancoregCallbacks(''toggle_off'')', ...
+        'string', 'OFF');
 
-    uicontrol(fg, 'style', 'text', 'string', 'TARGET IMAGE', 'position', [370 780 80 019] .* WS, 'Fontsize', 10);
-    uicontrol(fg, 'style', 'text', 'string', targetimage, 'position', [370 760 220 019] .* WS, 'Fontsize', 10);
+    mancoregvar.htoggle_on = uicontrol(fg, ...
+        'style', 'radiobutton', ...
+        'position', ...
+        [530 100 opt.largeFontBoxWidth opt.largeFontBoxHeight] .* opt.windowScale, ...
+        'Value', 0, ...
+        'Callback', 'mancoregCallbacks(''toggle_on'')', ...
+        'string', 'ON');
 
-    uicontrol(fg, 'style', 'text', 'string', 'SOURCE IMAGE', 'position', [370 415 80 019] .* WS, 'Fontsize', 10);
-    uicontrol(fg, 'style', 'text', 'string', sourceimage, 'position', [370 395 220 019] .* WS, 'Fontsize', 10);
+    % "Reset transformation" pushbutton
 
-    % 1.b Transformation matrix
+    mancoregvar.hreset = uicontrol(fg, ...
+        'style', 'pushbutton', ...
+        'position', [370 75 220 opt.largeFontBoxHeight] .* opt.windowScale, ...
+        'String', 'Reset transformation', ...
+        'Callback', 'mancoregCallbacks(''reset'')');
 
-    uicontrol(fg, 'style', 'text', 'string', 'transf.', 'position', [370 360 40 015] .* WS, 'Fontsize', 10);
-    mancoregvar.hmat_1_1 = uicontrol(fg, 'style', 'text', 'string', '1.00', 'position', [415 360 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_1_2 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [460 360 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_1_3 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [505 360 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_1_4 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [550 360 40 015] .* WS, 'Fontsize', 8);
+    % "Apply transformation" pushbutton
 
-    mancoregvar.hmat_2_1 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [415 340 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_2_2 = uicontrol(fg, 'style', 'text', 'string', '1.00', 'position', [460 340 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_2_3 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [505 340 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_2_4 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [550 340 40 015] .* WS, 'Fontsize', 8);
-
-    mancoregvar.hmat_3_1 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [415 320 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_3_2 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [460 320 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_3_3 = uicontrol(fg, 'style', 'text', 'string', '1.00', 'position', [505 320 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_3_4 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [550 320 40 015] .* WS, 'Fontsize', 8);
-
-    mancoregvar.hmat_4_1 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [415 300 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_4_2 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [460 300 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_4_3 = uicontrol(fg, 'style', 'text', 'string', '0.00', 'position', [505 300 40 015] .* WS, 'Fontsize', 8);
-    mancoregvar.hmat_4_4 = uicontrol(fg, 'style', 'text', 'string', '1.00', 'position', [550 300 40 015] .* WS, 'Fontsize', 8);
-
-    %% 2. Rotation sliders
-
-    mancoregvar.hpitch =    uicontrol(fg, 'style', 'slider', 'position', [430 250 100 019] .* WS, 'Value', 0, ...
-        'min', -pi, 'max', pi, 'sliderstep', [0.01 0.01], 'Callback', 'mancoreg_callbacks(''move'')');
-    mancoregvar.hroll  =    uicontrol(fg, 'style', 'slider', 'position', [430 225 100 019] .* WS, 'Value', 0, ...
-        'min', -pi, 'max', pi, 'sliderstep', [0.01 0.01], 'Callback', 'mancoreg_callbacks(''move'')');
-    mancoregvar.hyaw   =    uicontrol(fg, 'style', 'slider', 'position', [430 200 100 019] .* WS, 'Value', 0, ...
-        'min', -pi, 'max', pi, 'sliderstep', [0.01 0.01], 'Callback', 'mancoreg_callbacks(''move'')');
-
-    uicontrol(fg, 'style', 'text', 'string', 'PITCH', 'position', [370 250 60 019] .* WS, 'Fontsize', 10);
-    uicontrol(fg, 'style', 'text', 'string', 'ROLL', 'position', [370 225 60 019] .* WS, 'Fontsize', 10);
-    uicontrol(fg, 'style', 'text', 'string', 'YAW', 'position', [370 200 60 019] .* WS, 'Fontsize', 10);
-
-    mancoregvar.hpitch_val = uicontrol(fg, 'style', 'text', 'string', '0', 'position', [530 250 60 019] .* WS, 'Fontsize', 10);
-    mancoregvar.hroll_val = uicontrol(fg, 'style', 'text', 'string', '0', 'position', [530 225 60 019] .* WS, 'Fontsize', 10);
-    mancoregvar.hyaw_val = uicontrol(fg, 'style', 'text', 'string', '0', 'position', [530 200 60 019] .* WS, 'Fontsize', 10);
-
-    %% 3. Translation sliders
-
-    mancoregvar.hx =    uicontrol(fg, 'style', 'slider', 'position', [430 175 100 019] .* WS, 'Value', 0, ...
-        'min', -500, 'max', 500, 'sliderstep', [0.01 0.01], 'Callback', 'mancoreg_callbacks(''move'')');
-    mancoregvar.hy =    uicontrol(fg, 'style', 'slider', 'position', [430 150 100 019] .* WS, 'Value', 0, ...
-        'min', -500, 'max', 500, 'sliderstep', [0.01 0.01], 'Callback', 'mancoreg_callbacks(''move'')');
-    mancoregvar.hz =    uicontrol(fg, 'style', 'slider', 'position', [430 125 100 019] .* WS, 'Value', 0, ...
-        'min', -500, 'max', 500, 'sliderstep', [0.01 0.01], 'Callback', 'mancoreg_callbacks(''move'')');
-
-    uicontrol(fg, 'style', 'text', 'string', 'X', 'position', [370 175 60 019] .* WS, 'Fontsize', 10);
-    uicontrol(fg, 'style', 'text', 'string', 'Y', 'position', [370 150 60 019] .* WS, 'Fontsize', 10);
-    uicontrol(fg, 'style', 'text', 'string', 'Z', 'position', [370 125 60 019] .* WS, 'Fontsize', 10);
-
-    mancoregvar.hx_val = uicontrol(fg, 'style', 'text', 'string', '0', 'position', [530 175 60 019] .* WS, 'Fontsize', 10);
-    mancoregvar.hy_val = uicontrol(fg, 'style', 'text', 'string', '0', 'position', [530 150 60 019] .* WS, 'Fontsize', 10);
-    mancoregvar.hz_val = uicontrol(fg, 'style', 'text', 'string', '0', 'position', [530 125 60 019] .* WS, 'Fontsize', 10);
-
-    %% 4. Source/target display toggle
-
-    mancoregvar.htoggle_off =    uicontrol(fg, 'style', 'radiobutton', 'position', [470 100 040 019] .* WS, 'Value', 1, ...
-        'Callback', 'mancoreg_callbacks(''toggle_off'')', 'string', 'OFF');
-    mancoregvar.htoggle_on  =    uicontrol(fg, 'style', 'radiobutton', 'position', [530 100 060 019] .* WS, 'Value', 0, ...
-        'Callback', 'mancoreg_callbacks(''toggle_on'')', 'string', 'ON');
-
-    %% 5. "Reset transformation" pushbutton
-
-    mancoregvar.hreset =    uicontrol(fg, 'style', 'pushbutton', 'position', [370 75 220 019] .* WS, 'String', 'Reset transformation', ...
-        'Callback', 'mancoreg_callbacks(''reset'')');
-
-    %% 6. "Apply transformation" pushbutton
-
-    mancoregvar.hwrite =    uicontrol(fg, 'style', 'pushbutton', 'position', [370 50 220 019] .* WS, 'String', 'Apply transformation', ...
-        'Callback', 'mancoreg_callbacks(''apply'')');
+    mancoregvar.hwrite = uicontrol(fg, 'style', 'pushbutton', ...
+        'position', [370 50 220 opt.largeFontBoxHeight] .* opt.windowScale, ...
+        'String', 'Apply transformation', ...
+        'Callback', 'mancoregCallbacks(''apply'')');
 
     %% Fill in "transf." fields
 
-    mancoreg_callbacks('plotmat');
+    mancoregCallbacks('plotmat');
 
     return
+
+end
+
+function initTitleAndBoxes(opt, fg, targetimage, sourceimage)
+
+    windowScale = opt.windowScale;
+    fontSize = opt.largeFontSize;
+    height = opt.largeFontBoxHeight;
+
+    uicontrol(fg, ...
+        'style', 'text', ...
+        'string', 'Manual coregistration tool', ...
+        'position', [200 825 300 30] .* windowScale, ...
+        'Fontsize', 16, 'backgroundcolor', [1 1 1]);
+
+    uicontrol(fg, ...
+        'style', 'frame', ...
+        'position', [360 550 240 250] .* windowScale);
+    uicontrol(fg, 'style', 'frame', ...
+        'position', [360 40 240 410] .* windowScale);
+
+    addTextToUI( ...
+        fg, 'TARGET IMAGE', ...
+        [370 760 100 height], opt, fontSize);
+    addTextToUI( ...
+        fg, targetimage, ...
+        [370 700 220 height * 3], opt, fontSize);
+
+    addTextToUI( ...
+        fg, 'SOURCE IMAGE', ...
+        [370 415 100 height], opt, fontSize);
+    addTextToUI( ...
+        fg, sourceimage, ...
+        [370 395 220 height], opt, fontSize);
+
+end
+
+function mancoregvar = initTransMat(mancoregvar, opt, fg)
+
+    fontSize = opt.smallFontSize;
+
+    width = opt.smallFontBoxWidth;
+    height = opt.smallFontBoxHeight;
+
+    colPosition = 415:45:550;
+    rowPosition = 360:-20:300;
+
+    addTextToUI( ...
+        fg, 'transf.', ...
+        [370 360 width height], opt, opt.largeFontSize);
+
+    for iRow = 1:numel(rowPosition)
+
+        for iCol = 1:numel(colPosition)
+
+            xPos = colPosition(iCol);
+            yPos = rowPosition(iRow);
+            fieldName = sprintf('hmat_%i_%i', iRow, iCol);
+
+            stringToUse = '0.00';
+            if iRow == iCol
+                stringToUse = '1.00';
+            end
+
+            mancoregvar = addTextToUI( ...
+                fg, stringToUse, ...
+                [xPos yPos width height], opt, fontSize, ...
+                mancoregvar, fieldName);
+
+        end
+    end
+
+end
+
+function mancoregvar = initRotationSlider(mancoregvar, opt, fg)
+
+    windowScale = opt.windowScale;
+    fontSize = opt.largeFontSize;
+    height = opt.largeFontBoxHeight;
+    width = opt.largeFontBoxWidth;
+
+    sliderMin = opt.rotationSliderMin;
+    sliderMax = opt.rotationSliderMax;
+
+    sliderStep = opt.sliderStep;
+
+    % set sliders
+    mancoregvar.hpitch = uicontrol(fg, 'style', 'slider', ...
+        'position', [430 250 100 height] .* windowScale, ...
+        'Value', 0, ...
+        'min', sliderMin, ...
+        'max', sliderMax, ...
+        'sliderstep', sliderStep, ...
+        'Callback', 'mancoregCallbacks(''move'')');
+    mancoregvar.hroll = uicontrol(fg, 'style', 'slider', ...
+        'position', [430 225 100 height] .* windowScale, ...
+        'Value', 0, ...
+        'min', sliderMin, ...
+        'max', sliderMax, ...
+        'sliderstep', sliderStep, ...
+        'Callback', 'mancoregCallbacks(''move'')');
+    mancoregvar.hyaw = uicontrol(fg, 'style', 'slider', ...
+        'position', [430 200 100 height] .* windowScale, ...
+        'Value', 0, ...
+        'min', sliderMin, ...
+        'max', sliderMax, ...
+        'sliderstep', sliderStep, ...
+        'Callback', 'mancoregCallbacks(''move'')');
+
+    % display text
+    xPos = 370;
+
+    addTextToUI( ...
+        fg, 'PITCH', ...
+        [xPos 250 width height], opt, fontSize);
+    addTextToUI( ...
+        fg, 'ROLL', ...
+        [xPos 225 width height], opt, fontSize);
+    addTextToUI( ...
+        fg, 'YAW', ...
+        [xPos 200 width height], opt, fontSize);
+
+    % display value
+    xPos = 530;
+
+    mancoregvar = addTextToUI( ...
+        fg, '0', ...
+        [xPos 250 width height], opt, fontSize, ...
+        mancoregvar, 'hpitch_val');
+    mancoregvar = addTextToUI( ...
+        fg, '0', ...
+        [xPos 225 width height], opt, fontSize, ...
+        mancoregvar, 'hroll_val');
+    mancoregvar = addTextToUI( ...
+        fg, '0', ...
+        [xPos 200 width height], opt, fontSize, ...
+        mancoregvar, 'hyaw_val');
+
+end
+
+function mancoregvar = initTranslationSlider(mancoregvar, opt, fg)
+
+    windowScale = opt.windowScale;
+    fontSize = opt.largeFontSize;
+    height = opt.largeFontBoxHeight;
+    width = opt.largeFontBoxWidth;
+
+    sliderMin = opt.translationSliderMin;
+    sliderMax = opt.translationSliderMax;
+
+    sliderStep = opt.sliderStep;
+
+    % add sliders
+    xPos = 430;
+
+    mancoregvar.hx = uicontrol(fg, ...
+        'style', 'slider', ...
+        'position', [xPos 175 100 height] .* windowScale, ...
+        'Value', 0, ...
+        'min', sliderMin, ...
+        'max', sliderMax, ...
+        'sliderstep', sliderStep, ...
+        'Callback', 'mancoregCallbacks(''move'')');
+    mancoregvar.hy = uicontrol(fg, ...
+        'style', 'slider', ...
+        'position', [xPos 150 100 height] .* windowScale, ...
+        'Value', 0, ...
+        'min', sliderMin, ...
+        'max', sliderMax, ...
+        'sliderstep', sliderStep, ...
+        'Callback', 'mancoregCallbacks(''move'')');
+    mancoregvar.hz = uicontrol(fg, ...
+        'style', 'slider', ...
+        'position', [xPos 125 100 height] .* windowScale, ...
+        'Value', 0, ...
+        'min', sliderMin, ...
+        'max', sliderMax, ...
+        'sliderstep', sliderStep, ...
+        'Callback', 'mancoregCallbacks(''move'')');
+
+    % display text
+    xPos = 370;
+
+    addTextToUI( ...
+        fg, 'X', ...
+        [xPos 175 width height], opt, fontSize);
+    addTextToUI( ...
+        fg, 'Y', ...
+        [xPos 150 width height], opt, fontSize);
+    addTextToUI( ...
+        fg, 'Z', ...
+        [xPos 125 width height], opt, fontSize);
+
+    % display value
+    xPos = 530;
+
+    mancoregvar = addTextToUI( ...
+        fg, '0', ...
+        [xPos 175 width height], opt, fontSize, ...
+        mancoregvar, 'hx_val');
+    mancoregvar = addTextToUI( ...
+        fg, '0', ...
+        [xPos 150 width height], opt, fontSize, ...
+        mancoregvar, 'hy_val');
+    mancoregvar = addTextToUI( ...
+        fg, '0', ...
+        [xPos 125 width height], opt, fontSize, ...
+        mancoregvar, 'hz_val');
+
+end
+
+function mancoregvar = addTextToUI(varargin)
+
+    if numel(varargin) == 7
+        [fg, textString, position, opt, fontSize, mancoregvar, fieldName] = deal(varargin{:});
+    elseif numel(varargin) == 5
+        [fg, textString, position, opt, fontSize] = deal(varargin{:});
+        fieldName = [];
+    end
+
+    handle = uicontrol(fg, ...
+        'style', 'text', ...
+        'string', textString, ...
+        'position', position .* opt.windowScale, ...
+        'Fontsize', fontSize);
+
+    if ~isempty(fieldName)
+        mancoregvar.(fieldName) = handle;
+    end
+
+end
