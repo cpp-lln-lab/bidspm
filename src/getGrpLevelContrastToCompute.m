29cbd7b8ec138db3c7318a49b699e269f22f67d9
Remi Gau <remi_gau@hotmail.com>
2020-09-18 17:42:05 +0200

    refactor bids_RFX

---
+++ src/getGrpLevelContrastToCompute.m
@@ -0,0 +1,15 @@
+function grpLvlCon = getGrpLevelContrastToCompute(opt, isMVPA)
+
+    model = spm_jsonread(opt.model.univariate.file);
+    if isMVPA
+        model = spm_jsonread(opt.model.multivariate.file);
+    end
+
+    for iStep = 1:length(model.Steps)
+        if strcmp(model.Steps{iStep}.Level, 'dataset')
+            grpLvlCon = model.Steps{iStep}.AutoContrasts;
+            break
+        end
+    end
+
+end
---
+++ src/spmBatching/setBatchSmoothConImages.m
@@ -0,0 +1,37 @@
+% (C) Copyright 2019 CPP BIDS SPM-pipeline developpers
+
+function matlabbatch = setBatchSmoothConImages(group, funcFWHM, conFWHM, opt, isMVPA)
+
+    counter = 0;
+
+    matlabbatch = {};
+
+    %% Loop through the groups, subjects, and sessions
+    for iGroup = 1:length(group)
+
+        groupName = group(iGroup).name;
+
+        for iSub = 1:group(iGroup).numSub
+
+            counter = counter + 1;
+
+            subNumber = group(iGroup).subNumber{iSub};
+
+            printProcessingSubject(groupName, iSub, subNumber);
+
+            ffxDir = getFFXdir(subNumber, funcFWHM, opt, isMVPA);
+
+            conImg = spm_select('FPlist', ffxDir, '^con*.*nii$');
+            matlabbatch{counter}.spm.spatial.smooth.data = cellstr(conImg); %#ok<*AGROW>
+
+            % Define how much smoothing is required
+            matlabbatch{counter}.spm.spatial.smooth.fwhm = ...
+                [conFWHM conFWHM conFWHM];
+            matlabbatch{counter}.spm.spatial.smooth.dtype = 0;
+            matlabbatch{counter}.spm.spatial.smooth.prefix = [ ...
+                spm_get_defaults('smooth.prefix'), num2str(conFWHM)];
+
+        end
+    end
+
+end
