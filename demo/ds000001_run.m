7cfb077f483abd52a1af719930b5020e02b845c5
Remi Gau <remi_gau@hotmail.com>
2020-09-26 20:18:25 +0200

    datalad openneuro demos

---
+++ demo/ds000001_run.m
@@ -0,0 +1,32 @@
+% runDs00014
+
+clear;
+clc;
+
+% Smoothing to apply
+FWHM = 6;
+
+% directory with this script becomes the current directory
+WD = fileparts(mfilename('fullpath'));
+
+% we add all the subfunctions that are in the sub directories
+addpath(fullfile(WD, '..'));
+addpath(genpath(fullfile(WD, '..', 'src')));
+addpath(genpath(fullfile(WD, '..', 'lib')));
+
+
+%% Set options
+opt = ds000001_getOption();
+
+checkDependencies();
+
+%% Run batches
+isMVPA = 0;
+
+% bidsCopyRawFolder(opt, 1);
+% bidsSTC(opt);
+bidsSpatialPrepro(opt);
+bidsSmoothing(FWHM, opt);
+% bidsFFX('specifyAndEstimate', opt, FWHM, isMVPA);
+% bidsFFX('contrasts', opt, FWHM, isMVPA);
+% bidsResults(opt, FWHM, [], isMVPA);
