405280ff745684942b17548bf0fd75555d07306a
Remi Gau <remi_gau@hotmail.com>
2020-10-22 21:49:33 +0200

    update getFFXdir

--- src/getFFXdir.m
+++ src/getFFXdir.m
@@ -1,19 +1,17 @@
 % (C) Copyright 2019 CPP BIDS SPM-pipeline developpers
 
-function ffxDir = getFFXdir(subID, funcFWFM, opt, isMVPA)
-    % ffxDir = getFFXdir(subID, funcFWFM, opt, isMVPA)
+function ffxDir = getFFXdir(subID, funcFWFM, opt)
+    % ffxDir = getFFXdir(subID, funcFWFM, opt)
     %
     % sets the name the FFX directory and creates it if it does not exist
     %
     %
 
-    mvpaSuffix = setMvpaSuffix(isMVPA);
-
     ffxDir = fullfile(opt.derivativesDir, ...
                       ['sub-', subID], ...
                       'stats', ...
                       ['ffx_task-', opt.taskName], ...
-                      ['ffx_FWHM-', num2str(funcFWFM), mvpaSuffix]);
+                      ['ffx_space-' opt.space '_FWHM-', num2str(funcFWFM)]);
 
     if ~exist(ffxDir, 'dir')
         mkdir(ffxDir);
--- tests/test_getFFXdir.m
+++ tests/test_getFFXdir.m
@@ -8,19 +8,20 @@ end
 
 function test_getFFXdirBasic()
 
-    isMVPA = false;
     funcFWFM = 0;
     subID = '01';
     opt.derivativesDir = fullfile(fileparts(mfilename('fullpath')), 'dummyData');
     opt.taskName = 'funcLocalizer';
 
     opt = setDerivativesDir(opt);
+    
+    opt = checkOptions(opt);
 
     expectedOutput = fullfile(fileparts(mfilename('fullpath')), 'dummyData', 'derivatives', ...
                               'SPM12_CPPL', 'sub-01', 'stats', 'ffx_task-funcLocalizer', ...
-                              'ffx_FWHM-0');
+                              'ffx_space-MNI_FWHM-0');
 
-    ffxDir = getFFXdir(subID, funcFWFM, opt, isMVPA);
+    ffxDir = getFFXdir(subID, funcFWFM, opt);
 
     assertEqual(exist(expectedOutput, 'dir'), 7);
 
@@ -28,19 +29,21 @@ end
 
 function test_getFFXdirMvpa()
 
-    isMVPA = true;
     funcFWFM = 6;
     subID = '02';
     opt.derivativesDir = fullfile(fileparts(mfilename('fullpath')), 'dummyData');
     opt.taskName = 'nBack';
+    opt.space = 'individual';
 
     opt = setDerivativesDir(opt);
+    
+    opt = checkOptions(opt);
 
     expectedOutput = fullfile(fileparts(mfilename('fullpath')), 'dummyData', 'derivatives', ...
                               'SPM12_CPPL', 'sub-02', 'stats', 'ffx_task-nBack', ...
-                              'ffx_FWHM-6_MVPA');
+                              'ffx_space-individual_FWHM-6');
 
-    ffxDir = getFFXdir(subID, funcFWFM, opt, isMVPA);
+    ffxDir = getFFXdir(subID, funcFWFM, opt);
 
     assertEqual(exist(expectedOutput, 'dir'), 7);
 
