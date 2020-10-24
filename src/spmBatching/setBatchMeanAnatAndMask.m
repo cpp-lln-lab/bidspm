399f6c42b301360f4ef0ef477414e8d030b991f6
Remi Gau <remi_gau@hotmail.com>
2020-10-24 09:16:05 +0200

    refactor selecting anat file

--- src/spmBatching/setBatchMeanAnatAndMask.m
+++ src/spmBatching/setBatchMeanAnatAndMask.m
@@ -23,30 +23,16 @@ function matlabbatch = setBatchMeanAnatAndMask(opt, funcFWHM, rfxDir)
 
             printProcessingSubject(groupName, iSub, subID);
 
-            %% STRUCTURAL
-            struct = spm_BIDS(BIDS, 'data', ...
-                              'sub', subID, ...
-                              'type', 'T1w');
-            % we assume that the first T1w is the correct one (could be an
-            % issue for data set with more than one
-            struct = struct{1};
+            %% STRUCTURAL            
+            [anatImage, anatDataDir] = getAnatFilename(BIDS, subID, opt);
 
-            [subStrucDataDir, file, ext] = spm_fileparts(struct);
-
-            % get filename of the orginal file (drop the gunzip extension)
-            if strcmp(ext, '.gz')
-                fileName = file;
-            elseif strcmp(ext, '.nii')
-                fileName = [file ext];
-            end
-
-            files = inputFileValidation( ...
-                                        subStrucDataDir, ...
+            anatImage = validationInputFile( ...
+                                        anatDataDir, ...
+                                        anatImage, ...
                                         [spm_get_defaults('normalise.write.prefix'), ...
-                                         spm_get_defaults('deformations.modulate.prefix')], ...
-                                        fileName);
+                                         spm_get_defaults('deformations.modulate.prefix')]);
 
-            matlabbatch{1}.spm.util.imcalc.input{subCounter, :} = files{1};
+            matlabbatch{1}.spm.util.imcalc.input{subCounter, :} = anatImage{1};
 
             %% Mask
             ffxDir = getFFXdir(subID, funcFWHM, opt);
@@ -82,18 +68,18 @@ function matlabbatch = setBatchMeanAnatAndMask(opt, funcFWHM, rfxDir)
     matlabbatch{1}.spm.util.imcalc.output = 'meanAnat.nii';
     matlabbatch{1}.spm.util.imcalc.outdir{:} = rfxDir;
     matlabbatch{1}.spm.util.imcalc.expression = meanStruct_equation;
-    matlabbatch{1}.spm.util.imcalc.options.dmtx = 0;
-    matlabbatch{1}.spm.util.imcalc.options.mask = 0;
-    matlabbatch{1}.spm.util.imcalc.options.interp = 1;
-    matlabbatch{1}.spm.util.imcalc.options.dtype = 4;
+%     matlabbatch{1}.spm.util.imcalc.options.dmtx = 0;
+%     matlabbatch{1}.spm.util.imcalc.options.mask = 0;
+%     matlabbatch{1}.spm.util.imcalc.options.interp = 1;
+%     matlabbatch{1}.spm.util.imcalc.options.dtype = 4;
 
     %% The mean mask will be saved in the RFX folder
     matlabbatch{2}.spm.util.imcalc.output = 'meanMask.nii';
     matlabbatch{2}.spm.util.imcalc.outdir{:} = rfxDir;
     matlabbatch{2}.spm.util.imcalc.expression = meanMask_equation;
-    matlabbatch{2}.spm.util.imcalc.options.dmtx = 0;
-    matlabbatch{2}.spm.util.imcalc.options.mask = 0;
-    matlabbatch{2}.spm.util.imcalc.options.interp = 1;
-    matlabbatch{2}.spm.util.imcalc.options.dtype = 4;
+%     matlabbatch{2}.spm.util.imcalc.options.dmtx = 0;
+%     matlabbatch{2}.spm.util.imcalc.options.mask = 0;
+%     matlabbatch{2}.spm.util.imcalc.options.interp = 1;
+%     matlabbatch{2}.spm.util.imcalc.options.dtype = 4;
 
 end
