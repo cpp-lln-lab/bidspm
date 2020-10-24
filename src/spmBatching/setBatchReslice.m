8655be060a09ae66b65de12ea3c01355aa5921f0
Remi Gau <remi_gau@hotmail.com>
2020-10-24 11:12:41 +0200

    add batch to reslice

---
+++ src/spmBatching/setBatchReslice.m
@@ -0,0 +1,14 @@
+% (C) Copyright 2019 CPP BIDS SPM-pipeline developpers
+
+function matlabbatch = setBatchReslice(referenceImg, sourceImages)
+    % matlabbatch = bidsSmoothing(referenceImg, sourceImages)
+    %
+    % referenceImg
+    %
+    % sourceImages: a cell
+    
+    matlabbatch = [];
+    matlabbatch{1}.spm.spatial.coreg.write.ref = {referenceImg};
+    matlabbatch{1}.spm.spatial.coreg.write.source = sourceImages;
+    
+end
