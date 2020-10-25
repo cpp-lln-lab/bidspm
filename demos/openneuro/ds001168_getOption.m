0e5ac7f23e89cc49c93578c4f64284484fbf5941
Remi Gau <remi_gau@hotmail.com>
2020-10-25 17:31:57 +0100

    add basic demo to show how to use fieldmap workflow

---
+++ demos/openneuro/ds001168_getOption.m
@@ -0,0 +1,27 @@
+% (C) Copyright 2019 CPP BIDS SPM-pipeline developpers
+
+function opt = ds001168_getOption()
+    % returns a structure that contains the options chosen by the user to run
+    % slice timing correction, pre-processing, FFX, RFX.
+
+    if nargin < 1
+        opt = [];
+    end
+
+    % The directory where the data are located
+    opt.dataDir = '/home/remi/openneuro/ds001168/raw';
+
+    % suject to run in each group
+    opt.subjects = {'01'};
+
+    % task to analyze
+    opt.taskName = 'rest';
+
+    opt.anatReference.type = 'T1w';
+    opt.anatReference.session = 1;
+
+    %% DO NOT TOUCH
+    opt = checkOptions(opt);
+%     saveOptions(opt);
+
+end
---
+++ demos/openneuro/ds001168_run.m
@@ -0,0 +1,50 @@
+% (C) Copyright 2020 CPP BIDS SPM-pipeline developpers
+
+% runDs001168
+
+clear;
+clc;
+close all;
+
+% Smoothing to apply
+FWHM = 6;
+
+% directory with this script becomes the current directory
+WD = fileparts(mfilename('fullpath'));
+
+% we add all the subfunctions that are in the sub directories
+addpath(fullfile(WD, '..'));
+addpath(genpath(fullfile(WD, '..', 'src')));
+addpath(genpath(fullfile(WD, '..', 'lib')));
+
+%% Set options
+opt = ds001168_getOption();
+
+% the line below allows to run preprocessing in "native" space.
+% - use realign and unwarp
+% - don't do normalization
+opt.space = 'individual';
+
+checkDependencies();
+
+%% Run batches
+
+% reportBIDS(opt);
+
+% bidsCopyRawFolder(opt, 1);
+
+bidsCreateVDM(opt);
+
+% bidsSTC(opt);
+%
+% bidsSpatialPrepro(opt);
+%
+% anatomicalQA(opt);
+% bidsResliceTpmToFunc(opt);
+% functionalQA(opt);
+%
+% bidsSmoothing(FWHM, opt);
+%
+% bidsFFX('specifyAndEstimate', opt, FWHM);
+% bidsFFX('contrasts', opt, FWHM);
+% bidsResults(opt, FWHM, []);
