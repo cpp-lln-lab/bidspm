9498b1623fc3d4a3c4b398b0090a7327bedc6d13
Remi Gau <remi_gau@hotmail.com>
2020-10-26 00:32:26 +0100

    update fmap workflow

---
+++ src/batches/setBatchCreateVDMs.m
@@ -0,0 +1,84 @@
+% (C) Copyright 2020 CPP BIDS SPM-pipeline developpers
+
+function matlabbatch = setBatchCreateVDMs(opt, BIDS, subID)
+
+    % TODO
+    % assumes all the fieldmap relate to the current task
+    % - use the "for" metadata field
+    % - implement for 'phase12', 'fieldmap', 'epi'
+
+    [sessions, nbSessions] = getInfo(BIDS, subID, opt, 'Sessions');
+
+    matlabbatch = [];
+    for iSes = 1:nbSessions
+
+        runs = spm_BIDS(BIDS, 'runs', ...
+                        'modality', 'fmap', ...
+                        'sub', subID, ...
+                        'ses', sessions{iSes});
+
+        for iRun = 1:numel(runs)
+
+            matlabbatch = setBatchComputeVDM(matlabbatch, 'phasediff');
+
+            % TODO
+            % - Move to getInfo
+            fmapFiles = spm_BIDS(BIDS, 'data', ...
+                                 'modality', 'fmap', ...
+                                 'sub', subID, ...
+                                 'ses', sessions{iSes}, ...
+                                 'run', runs{iRun});
+
+            phaseImage = fmapFiles{1};
+            matlabbatch{end}.spm.tools.fieldmap.calculatevdm.subj.data.presubphasemag.phase = ...
+                {phaseImage};
+
+            magnitudeImage = strrep(phaseImage, 'phasediff', 'magnitude1');
+            matlabbatch{end}.spm.tools.fieldmap.calculatevdm.subj.data.presubphasemag.magnitude = ...
+                {magnitudeImage};
+
+            metadata = spm_BIDS(BIDS, 'metadata', ...
+                                'modality', 'fmap', ...
+                                'sub', subID, ...
+                                'ses', sessions{iSes}, ...
+                                'run', runs{iRun});
+
+            echotimes =  1000 * [ ...
+                                 metadata.EchoTime1, ...
+                                 metadata.EchoTime2]; % in milliseconds
+            matlabbatch{end}.spm.tools.fieldmap.calculatevdm.subj.defaults.defaultsval.et = echotimes;
+
+            totalReadoutTime = getTotalReadoutTime(opt, BIDS, subID);
+
+        end
+
+    end
+
+end
+
+function totalReadoutTime = getTotalReadoutTime(opt, BIDS, subID)
+
+    metadata = spm_BIDS(BIDS, 'metadata', ...
+                        'type', 'bold', ...
+                        'sub', subID, ...
+                        'task', opt.taskName);
+
+    % func run metadata: if the fmap is applied to several
+    % runs we take the metadata of the first run it must
+    % applied to
+    if numel(metadata) > 1
+        metadata = metadata{1};
+    end
+
+    if isfield(metadata, 'TotalReadoutTime')
+        totalReadoutTime = metadata.TotalReadoutTime;
+
+    elseif isfield(metadata, 'RepetitionTime')
+        totalReadoutTime = metadata.RepetitionTime;
+
+    elseif isfield(metadata, 'EffectiveEchoSpacing')
+        totalReadoutTime = (metadata.NumberOfEchos - 1) ...
+            * metadata.EffectiveEchoSpacing;
+    end
+
+end
--- src/workflows/bidsCreateVDM.m
+++ src/workflows/bidsCreateVDM.m
@@ -3,48 +3,57 @@
 function bidsCreateVDM(opt)
     % bidsCreateVDM(opt)
     %
-    % adapted from spmup spmup_BIDS_preprocess (@ commit
+    % inspired from spmup spmup_BIDS_preprocess (@ commit
     % 198c980d6d7520b1a996f0e56269e2ceab72cc83)
-    
+
     if nargin < 1
         opt = [];
     end
     opt = loadAndCheckOptions(opt);
-    
+
     % load the subjects/Groups information and the task name
     [group, opt, BIDS] = getData(opt);
-    
+
     fprintf(1, ' FIELDMAP WORKFLOW\n');
-    
+
     %% Loop through the groups, subjects, and sessions
     for iGroup = 1:length(group)
-        
+
         groupName = group(iGroup).name;
-        
+
         for iSub = 1:group(iGroup).numSub
-            
+
             subID = group(iGroup).subNumber{iSub};
-            
+
             % TODO Move to getInfo
             types = spm_BIDS(BIDS, 'types', 'sub', subID);
-            
+
             if any(ismember(types, {'phase12', 'phasediff', 'fieldmap', 'epi'}))
-                
+
                 printProcessingSubject(groupName, iSub, subID);
-                
+
                 fprintf(1, ' FIELDMAP WORKFLOW: COREGISTERING FIELD MAPS TO FIRST FUNC IMAGE\n');
-                
+
                 matlabbatch = setBatchCoregistrationFmap(opt, BIDS, subID);
-                
-                saveMatlabBatch(matlabbatch, 'coregister_fmap', opt, subID);
-                
-                spm_jobman('run', matlabbatch);
-                
-                %             fprintf(1, ' FIELDMAP WORKFLOW: CREATING VDMs \n');
-                
+                %
+                %                 saveMatlabBatch(matlabbatch, 'coregister_fmap', opt, subID);
+
+                %                 spm_jobman('run', matlabbatch);
+
+                fprintf(1, ' FIELDMAP WORKFLOW: CREATING VDMs \n');
+
+                matlabbatch = setBatchCreateVDMs(opt, BIDS, subID);
+
+                saveMatlabBatch(matlabbatch, 'create_vdm', opt, subID);
+
+                %                 spm_jobman('run', matlabbatch);
+
+                % TODO
+                % delete temporary mean images ??
+
             end
-            
+
         end
-        
+
     end
 end
