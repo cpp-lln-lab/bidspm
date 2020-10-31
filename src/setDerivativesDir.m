e2610cd45fedc8078b58d6eebc28b6253cc3c27e
Remi Gau <remi_gau@hotmail.com>
2020-10-31 17:08:49 +0100

    update copyright dates

--- src/setDerivativesDir.m
+++ src/setDerivativesDir.m
@@ -1,4 +1,4 @@
-% (C) Copyright 2019 CPP BIDS SPM-pipeline developpers
+% (C) Copyright 2020 CPP BIDS SPM-pipeline developpers
 
 function opt = setDerivativesDir(opt)
   % derivativeDir sets the derivatives folder
--- src/workflows/bidsSegmentSkullStrip.m
+++ src/workflows/bidsSegmentSkullStrip.m
@@ -1,4 +1,4 @@
-% (C) Copyright 2019 CPP BIDS SPM-pipeline developpers
+% (C) Copyright 20120 CPP BIDS SPM-pipeline developpers
 
 function bidsSegmentSkullStrip(opt)
   %
