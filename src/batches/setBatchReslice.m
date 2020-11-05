d5dd0f05979e85cbef0f0c85fb95409db538f86d
Remi Gau <remi_gau@hotmail.com>
2020-11-05 09:22:21 +0100

    make setBathReslice more general

--- src/batches/setBatchReslice.m
+++ src/batches/setBatchReslice.m
@@ -1,14 +1,23 @@
 % (C) Copyright 2020 CPP BIDS SPM-pipeline developers
 
-function matlabbatch = setBatchReslice(referenceImg, sourceImages)
+function matlabbatch = setBatchReslice(matlabbatch, referenceImg, sourceImages)
   % matlabbatch = bidsSmoothing(referenceImg, sourceImages)
   %
   % referenceImg
   %
   % sourceImages: a cell
-
-  matlabbatch = [];
-  matlabbatch{1}.spm.spatial.coreg.write.ref = {referenceImg};
-  matlabbatch{1}.spm.spatial.coreg.write.source = sourceImages;
+  
+  if ischar(referenceImg)
+    matlabbatch{end+1}.spm.spatial.coreg.write.ref = {referenceImg};
+    
+  elseif isstruct(referenceImg)
+  end
+  
+  if iscell(sourceImages)
+    matlabbatch{1}.spm.spatial.coreg.write.source = sourceImages;
+    
+  elseif isstruct(sourceImages)
+  end
+  
 
 end
