6a5e353c83a17dc9dcb1355ccba3005c2d348236
Remi Gau <remi_gau@hotmail.com>
2020-11-05 09:35:07 +0100

    add smoke system test for bidsSegmentSkullStrip

---
+++ tests/test_bidsSegmentSkullStrip.m
@@ -0,0 +1,29 @@
+function test_suite = test_bidsSegmentSkullStrip %#ok<*STOUT>
+  try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+    test_functions = localfunctions(); %#ok<*NASGU>
+  catch % no problem; early Matlab versions can use initTestSuite fine
+  end
+  initTestSuite;
+end
+
+function test_bidsSegmentSkullStripBasic()
+
+  % smoke test
+
+  % directory with this script becomes the current directory
+  opt.dataDir = fullfile( ...
+                         fileparts(mfilename('fullpath')), ...
+                         '..', 'demos',  'MoAE', 'output', 'MoAEpilot');
+  % task to analyze
+  opt.taskName = 'auditory';
+  opt = checkOptions(opt);
+
+
+  %% Run batches
+  checkDependencies();
+  reportBIDS(opt);
+  bidsCopyRawFolder(opt, 1);
+  bidsSegmentSkullStrip(opt);
+
+
+end
