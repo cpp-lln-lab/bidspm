353b2a8ce82a2529d019f0caad528bceebfd18b5
Remi Gau <remi_gau@hotmail.com>
2020-11-06 11:49:17 +0100

    create options for system tests

--- manualTests/systemTestDS114.m
+++ manualTests/systemTestDS114.m
@@ -19,25 +19,14 @@ clc;
 % Smoothing to apply
 FWHM = 6;
 
-% URL of the data set to download
-URL = 'http://www.fil.ion.ucl.ac.uk/spm/download/data/MoAEpilot/MoAEpilot.bids.zip';
-
 % directory with this script becomes the current directory
-WD = fullfile(fileparts(mfilename('fullpath')), '..', 'demos', 'MoAE');
+WD = fullfile(fileparts(mfilename('fullpath')), '..', 'demos', 'openneuro');
 cd(WD);
 
 % we add all the subfunctions that are in the sub directories
 addpath(genpath(fullfile(WD, '..', '..', 'src')));
 addpath(genpath(fullfile(WD, '..', '..', 'lib')));
 
-%% Get data
-fprintf('%-10s:', 'Downloading dataset...');
-urlwrite(URL, 'MoAEpilot.zip');
-fprintf(1, ' Done\n\n');
-
-fprintf('%-10s:', 'Unzipping dataset...');
-unzip('MoAEpilot.zip', fullfile(WD, 'output'));
-fprintf(1, ' Done\n\n');
 
 checkDependencies();
 
@@ -45,17 +34,17 @@ checkDependencies();
 delete(fullfile(pwd, 'options_task-*date-*.json'));
 
 optionsFilesList = {...
-  'options_task-auditory_unwarp-0.json'; ...
-  'options_task-auditory_unwarp-0.json'; ...
-  'options_task-auditory_unwarp-0_space-individual.json'; ...
-  'options_task-auditory_space-individual.json'};
+  'options_task-linebisection_unwarp-0.json'; ...
+  'options_task-linebisection_unwarp-0.json'; ...
+  'options_task-linebisection_unwarp-0_space-individual.json'; ...
+  'options_task-linebisection_space-individual.json'};
 
 % run the pipeline with different options
 for iOption = 1:size(optionsFilesList,1)
   
   fprintf(1, repmat('\n', 1, 5));
   
-  optionJsonFile = optionsFilesList{iOption};
+  optionJsonFile = fullfile(WD, 'options', optionsFilesList{iOption});
   opt = loadAndCheckOptions(optionJsonFile);
 
   %% Run batches
