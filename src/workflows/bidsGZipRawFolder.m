ac887bd64637674856a308f28ef7a24b17adb57b
marcobarilari <marcobarilr@gmail.com>
2020-11-12 15:14:37 +0100

    update workflow script to gzip

--- src/workflows/bidsGZipRawFolder.m
+++ src/workflows/bidsGZipRawFolder.m
@@ -1,6 +1,6 @@
 % (C) Copyright 2020 CPP BIDS SPM-pipeline developers
 
-function bidsGZipRawFolder(opt, deleteUnzippedNii, modalitiesToZip)
+function bidsGZipRawFolder(optSource, keepUnzippedNii)
 
   %
   % GZip the nii files in a ``raw`` bids folders from the. It will do it independently of the task.
@@ -11,39 +11,32 @@ function bidsGZipRawFolder(opt, deleteUnzippedNii, modalitiesToZip)
   %                     [deleteUnzippedNii = true,] ...
   %                     [modalitiesToZip = {'anat', 'func', 'fmap'}])
   %
-  % :param opt: structure or json filename containing the options. See
-  %             ``checkOptions()`` and ``loadAndCheckOptions()``.
+  % :param optSource: The structure that contains the options set by the user to run the batch
+  %                   workflow for source processing
   % :type opt: structure
-  % :param deleteUnzippedNii: will delete the original ``.nii`` if set to ``true``
-  % :type deleteUnzippedNii: boolean
-  % :param modalitiesToZip:
-  % :type modalitiesToZip: cell
-  
-  %% input variables default values
+  % :param keepUnzippedNii: will keep the original ``.nii`` if set to ``true``. Default is false
+  % :type keepUnzippedNii: boolean
 
-  if nargin < 3 || isempty(modalitiesToZip)
-    % Will only copy those modalities if they exist
-    modalitiesToZip = {'anat', 'func', 'fmap'};
-  end
+  %% input variables default values
+  
+  optSource.dataDir = '/Users/barilari/Desktop/data_temp/raw_temp';
 
   if nargin < 2 || isempty(deleteUnzippedNii)
-    % delete the original zipped nii.gz
-    deleteUnzippedNii = true;
+    % delete the original unzipped .nii
+    keepUnzippedNii = false;
   end
 
-  % if input has no opt, load the opt.mat file
-  if nargin < 1 || isempty(opt)
-    opt = [];
-  end
-  opt = loadAndCheckOptions(opt);
-
-  cleanCrash();
-
+  tic;
+  
   printWorklowName('GZip data');
   
-  rawDir = opt.dataDir;
+  rawDir = optSource.dataDir;
 
-  unzippedNiifiles = spm_select('FPListRec', rawDir, '^.*.nii$');
+  unzippedNiifiles = cellstr(spm_select('FPListRec', rawDir, '^.*.nii$'));
   
+  matlabbatch = setBatchGZip(unzippedNiifiles, keepUnzippedNii);
+
+  spm_jobman('run', matlabbatch);
   
-  
+  toc;
+
