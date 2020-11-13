eefa8ae8e3e64061231a4cafec6fac1ca94da50d
marcobarilari <marcobarilr@gmail.com>
2020-11-13 12:24:46 +0100

    add code to test fun checkOptionsSource

--- tests/test_checkOptionsSource.m
+++ tests/test_checkOptionsSource.m
@@ -5,3 +5,47 @@ function test_suite = test_checkOptionsSource %#ok<*STOUT>
   end
   initTestSuite;
 end
+
+function test_checkOptionsSourceBasic()
+
+  optSource.nbDummies = 0;
+  optSource = checkOptionsSource(optSource);
+
+  expectedOptionsSource = defaultOptionsSource();
+  expectedOptionsSource.nbDummies = 0;
+
+  assertEqual(optSource, expectedOptionsSource);
+
+end
+
+function test_checkOptionsSourceDoNotOverwrite()
+
+  optSource.dataType = 666;
+  optSource.someExtraField = 'test';
+  optSource.nbDummies = 42;
+
+  optSource = checkOptionsSource(optSource);
+
+  assertEqual(optSource.dataType, 666);
+  assertEqual(optSource.someExtraField, 'test');
+  assertEqual(optSource.nbDummies, 42);
+
+end
+
+function expectedOptionsSource = defaultOptionsSource()
+
+  expectedOptionsSource.sourceDir = '';
+  
+  expectedOptionsSource.dataDir = '';
+
+  expectedOptionsSource.sequenceToIgnore = {};
+
+  expectedOptionsSource.dataType = 0;
+
+  expectedOptionsSource.zip = 0;
+
+  expectedOptionsSource.nbDummies = 0;
+
+  expectedOptionsSource.sequenceRmDummies = {};
+
+end
