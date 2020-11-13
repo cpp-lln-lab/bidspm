a80bd6bee089e77fac0298126efea0d57997e18f
marcobarilari <marcobarilr@gmail.com>
2020-11-13 12:13:00 +0100

    update documentation

--- src/defaults/checkOptionsSource.m
+++ src/defaults/checkOptionsSource.m
@@ -12,6 +12,16 @@ function optSource = checkOptionsSource(optSource)
   %                   to run the batch workflow for source processing
   %
   % :returns: - :optSource: (struc) The structure with any unset fields with the deaufalt values
+  %   
+  % OPTIONS (with their defaults):
+  %   - ``optSource.sourceDir = ''`` - The directory where the source data are located.
+  %   - ``optSource.dataDir = ''`` - The directory where the raw data to apply changes are located.
+  %   - ``optSource.sequenceToIgnore  = {}`` - The list of sequence(s) to ignore.
+  %   - ``optSource.dataType = 0`` - Data format conversion (0 is reccomended).
+  %   - ``optSource.zip = 0`` - Boolean to enable gzip of the new 4D file in ``convert3Dto4D``.
+  %   - ``optSource.nbDummies = 0`` - Number of volumes to discard ad dummies in ``convert3Dto4D``.
+  %   - ``optSource.sequenceRmDummies = {}`` - The list of sequence(s) where to discarding the 
+  %     dummies.
 
   fieldsToSet = setDefaultOptionSource();
 
