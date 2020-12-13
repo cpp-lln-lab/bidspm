3e0d4c091b4d072d836a524bf54d6a37b40652bf
marcobarilari <marcobarilr@gmail.com>
2020-12-13 15:01:57 +0100

    add elapsed time for single func and for global set of funcs as in the batch

--- src/utils/elapsedTime.m
+++ src/utils/elapsedTime.m
@@ -1,22 +1,31 @@
-function elapsedTime(input)
+function globalStart = elapsedTime(input, varargin)
 
 switch input
-    
-    case 'start' 
-        
-        tic
-    
-    
-    case 'stop' 
-        
-        fprintf('\n\n\n******** Done :) ********\n\n');
-        fprintf('elapsed time is: ')
+
+    case 'start'
+
+        tic;
+
+    case 'stop'
+
+        fprintf('\n\n\n********* Done :) *********\n\n');
+        fprintf(' elapsed time is: ')
         t=toc;
         disp(datestr(datenum(0,0,0,0,0,t),'HH:MM:SS'));
-        fprintf('\n*************************\n\n');
-        
-end
-    
+        fprintf('\n***************************\n\n');
 
-    
-   
+    case 'globalStart'
+
+        globalStart = tic;
+
+    case 'globalStop'
+
+        globalStart = varargin{1};
+        
+        fprintf('\n\n\n********* Pipeline done :) *********\n\n');
+        fprintf('  global elapsed time is: ')
+        t=toc(globalStart);
+        disp(datestr(datenum(0,0,0,0,0,t),'HH:MM:SS'));
+        fprintf('\n************************************\n\n');
+
+end
