bffab981929403955606a7738bcd61ebcd8049e1
Remi Gau <remi_gau@hotmail.com>
2021-03-02 15:32:13 +0100

    update extract data from sphere

--- src/roi/createROI.m
+++ src/roi/createROI.m
@@ -1,44 +1,85 @@
-function mask = createROI(globalRoiImage)
+function mask = createROI(type, varargin)
     
-    % roiImage: fullpath of the roiImage
+    % roiImage:
+    %           - fullpath of the roi image
+    %           - structure with:
+    %               sphere.location = location; % X Y Z coordinates in millimeters
+    %               sphere.radius = radius; % radius in millimeters
+    
+    mask = struct('roi', struct('XYZmm', ''));
+    
+    switch type
+        
+        case 'sphere'
+            
+%             LocationsToSample = varargin{1};
+            specification = varargin{1};
+            
+            xY.def = type;
+            xY.spec = specification.radius;
+            xY.xyz = specification.location;
+            
+            if size(xY.xyz,1)~=3
+                xY.xyz = xY.xyz';
+            end
+            
+            mask = spm_ROI(xY);
 
-    %% % global roi
-    mask.global.hdr = spm_vol(globalRoiImage);
+        case 'mask'
+            
+            roiImage = varargin{1};
+            
+            xY.def = type;
+            xY.spec = roiImage;
+           
+            mask = defineGlobalSearchSpace(mask, roiImage);
+            
+            % in real world coordinates
+            mask.global.XYZmm = returnXYZm(mask.global.hdr.mat, mask.global.XYZ);
+            
+            assert(size(mask.global.XYZmm, 2)==sum(mask.global.img(:)))
+            
+            LocationsToSample = mask.global.XYZmm;
+            
+            [~, mask.roi.XYZmm, j] = spm_ROI(xY, LocationsToSample);
+            mask.roi.XYZ = mask.global.XYZ(:,j);
+            mask.roi.size = size(mask.roi.XYZ, 2);
+  
+    end
+    
+
+    
+    %     %%
+    %     for i=1:length(Mask.ROI)
+    %         Mask.ROI(i).hdr = spm_vol(fullfile(RoiFolder, Mask.ROI(i).fname));
+    %     end
+    %
+    %
+    %     %% Combine masks
+    %     xY.def = 'mask';
+    %     for i=1:length(Mask.ROI)
+    %         xY.spec = fullfile(RoiFolder, Mask.ROI(i).fname);
+    %         [xY, Mask.ROI(i).XYZmm, j] = spm_ROI(xY, Mask.global.XYZmm);
+    %         Mask.ROI(i).XYZ = Mask.global.XYZ(:,j);
+    %         Mask.ROI(i).size = size(Mask.ROI(i).XYZ, 2);
+    %     end
+    
+end
+
+function mask = defineGlobalSearchSpace(mask, image)
+    
+    mask.global.hdr = spm_vol(image);
     mask.global.img = logical(spm_read_vols(mask.global.hdr));
     
     [X, Y, Z] = ind2sub(size(mask.global.img), find(mask.global.img));
     
     % XYZ format
-    mask.global.XYZ = [X'; Y'; Z']; 
+    mask.global.XYZ = [X'; Y'; Z'];
     mask.global.size = size(mask.global.XYZ, 2);
     
-    % voxel to world transformation
-    mask.global.XYZmm = mask.global.hdr.mat(1:3,:) ...
-        * [mask.global.XYZ; ones(1, mask.global.size)]; 
-    
-    
-    if nargin<2
-        xY.def = 'mask';
-        xY.spec = globalRoiImage;
-        [xY, mask.roi.XYZmm, j] = spm_ROI(xY, mask.global.XYZmm);
-        mask.roi.XYZ = mask.global.XYZ(:,j);
-        mask.roi.size = size(mask.roi.XYZ, 2);
-        return
-    end
-    
-%     %% 
-%     for i=1:length(Mask.ROI)
-%         Mask.ROI(i).hdr = spm_vol(fullfile(RoiFolder, Mask.ROI(i).fname));
-%     end
-%     
-%     
-%     %% Combine masks
-%     xY.def = 'mask';
-%     for i=1:length(Mask.ROI)
-%         xY.spec = fullfile(RoiFolder, Mask.ROI(i).fname);
-%         [xY, Mask.ROI(i).XYZmm, j] = spm_ROI(xY, Mask.global.XYZmm);
-%         Mask.ROI(i).XYZ = Mask.global.XYZ(:,j);
-%         Mask.ROI(i).size = size(Mask.ROI(i).XYZ, 2);
-%     end
-    
+end
+
+function XYZmm = returnXYZm(transformationMatrix, XYZ)
+    % "voxel to world transformation"
+    XYZmm = transformationMatrix(1:3,:) * [XYZ; ones(1, size(XYZ, 2))];
 end
--- src/roi/roi_script.m
+++ src/roi/roi_script.m
@@ -28,22 +28,53 @@ clc
 
 % gunzip('*.gz')
 
-probabiliyMap = fullfile(pwd, 'visual motion_association-test_z_FDR_0.01.nii');
+probabilityMap = fullfile(pwd, 'visual motion_association-test_z_FDR_0.01.nii');
 dataImage = fullfile(pwd, 'TStatistic.nii');
 
 % If needed reslice probability map to have same resolution as the data image
+%
 % resliceImg won't do anything if the 2 images have the same resolution
-reslicedProbabilityMap = resliceImages(dataImage, probabiliyMap);
+%
+% if you reed the data with spm_summarise, then the 2 images do not need the
+% same resolution.
+
+% reslicedProbabilityMap = resliceImages(dataImage, probabilityMap);
 
 % Threshold probability map into a binary mask 
 % to keep only values above a certain threshold
 threshold = 10;
-roiName = thresholdToMask(reslicedProbabilityMap, threshold);
+
+roiName = thresholdToMask(probabilityMap, threshold);
+% roiNameHighRes = thresholdToMask(reslicedProbabilityMap, threshold);
+
 
 %% Get ROI voxel coordinates and extract data
-mask = createROI(roiName);
-data = getRoiData(dataImage, mask);
+expected = spm_summarise(dataImage, roiName);
+
+% mask = createROI('mask', roiNameHighRes);
+% data = getRoiData(dataImage, mask);
+% assertEqual(data, expected)
+  
+return
 
 %% Get data from a sphere at a specific location
+% X Y Z coordinates in millimeters
+% location = [21.33 -94.83 1.40];  
+location = [-7.46 -31.01 7.78]; 
+
+% radius in millimeters
+radius = 1;
+
+sphere.location = location;
+sphere.radius = radius;
+
+imageToSample = dataImage;
+sphere = createROI('sphere', sphere);
+% data = getRoiData(dataImage, mask);
+expected = spm_summarise(dataImage, sphere);
 
 
+
+
+b = spm_summarise('beta_0001.nii',...
+      struct('def','sphere', 'spec',5, 'xyz',[10 20 30]'));
