daee136c8bc8adf3083311e4c17f230e5bbd4bf2
Michele MacLean <michele.maclean@umontreal.ca>
2021-03-08 11:57:03 -0500

    add DetectLesion batch

---
+++ src/batches/setBatchDetectLesion.m
@@ -0,0 +1,23 @@
+%-----------------------------------------------------------------------
+% Job saved on 08-Mar-2021 11:11:19 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1data = '<UNDEFINED>';
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1prior = {'C:\Users\michm\spm12\toolbox\ALI\Priors_extraClass\wc4prior0.nii'};
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1niti = 2;
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1thr_prob = 0.333333333333333;
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1thr_size = 0.8;
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1coregister = 1;
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1mask = {''};
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1vox = 2;
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1fwhm = [8 8 8];
+matlabbatch{2}.spm.tools.ali.outliers_detection.step3tissue.step3patients = '<UNDEFINED>';
+matlabbatch{2}.spm.tools.ali.outliers_detection.step3tissue.step3controls = '<UNDEFINED>';
+matlabbatch{2}.spm.tools.ali.outliers_detection.step3tissue.step3Alpha = 0.5;
+matlabbatch{2}.spm.tools.ali.outliers_detection.step3tissue.step3Lambda = -4;
+matlabbatch{2}.spm.tools.ali.outliers_detection.step3mask = {'C:\Users\michm\spm12\toolbox\ALI\Mask_image\mask_controls_vox2mm.nii'};
+matlabbatch{2}.spm.tools.ali.outliers_detection.step3mask_thr = 0;
+matlabbatch{2}.spm.tools.ali.outliers_detection.step3binary_thr = 0.3;
+matlabbatch{2}.spm.tools.ali.outliers_detection.step3binary_size = 0.8;
+matlabbatch{3}.spm.tools.ali.lesion_overlap.lom = '<UNDEFINED>';
