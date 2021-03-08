2d3164220826371e4e1cb8b2cd76b8a70c0c4dab
Michele MacLean <michele.maclean@umontreal.ca>
2021-03-08 14:39:04 -0500

    add 3 batches for lesion detection

---
+++ src/batches/setBatchLesionOverlapMap.m
@@ -0,0 +1,6 @@
+%-----------------------------------------------------------------------
+% Job saved on 08-Mar-2021 12:48:14 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.tools.ali.lesion_overlap.lom = '<UNDEFINED>';
--- src/batches/setBatchSegmentationDetectLesion.m
+++ src/batches/setBatchSegmentationDetectLesion.m
@@ -5,7 +5,7 @@
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.tools.ali.unified_segmentation.step1data = '<UNDEFINED>';
 spmDir = spm('dir');
-lesionPriorMap = fullfile(spmDir, 'toolbox', 'ALI', 'Priors_extraClass', 'wc4prior0.nii)
+lesionPriorMap = fullfile(spmDir, 'toolbox', 'ALI', 'Priors_extraClass', 'wc4prior0.nii');
 matlabbatch{1}.spm.tools.ali.unified_segmentation.step1prior = {lesionPriorMap};
 matlabbatch{1}.spm.tools.ali.unified_segmentation.step1niti = 2;
 matlabbatch{1}.spm.tools.ali.unified_segmentation.step1thr_prob = 0.333333333333333;
@@ -14,12 +14,3 @@ matlabbatch{1}.spm.tools.ali.unified_segmentation.step1coregister = 1;
 matlabbatch{1}.spm.tools.ali.unified_segmentation.step1mask = {''};
 matlabbatch{1}.spm.tools.ali.unified_segmentation.step1vox = 2;
 matlabbatch{1}.spm.tools.ali.unified_segmentation.step1fwhm = [8 8 8];
-matlabbatch{2}.spm.tools.ali.outliers_detection.step3tissue.step3patients = '<UNDEFINED>';
-matlabbatch{2}.spm.tools.ali.outliers_detection.step3tissue.step3controls = '<UNDEFINED>';
-matlabbatch{2}.spm.tools.ali.outliers_detection.step3tissue.step3Alpha = 0.5;
-matlabbatch{2}.spm.tools.ali.outliers_detection.step3tissue.step3Lambda = -4;
-matlabbatch{2}.spm.tools.ali.outliers_detection.step3mask = {'C:\Users\michm\spm12\toolbox\ALI\Mask_image\mask_controls_vox2mm.nii'};
-matlabbatch{2}.spm.tools.ali.outliers_detection.step3mask_thr = 0;
-matlabbatch{2}.spm.tools.ali.outliers_detection.step3binary_thr = 0.3;
-matlabbatch{2}.spm.tools.ali.outliers_detection.step3binary_size = 0.8;
-matlabbatch{3}.spm.tools.ali.lesion_overlap.lom = '<UNDEFINED>';
