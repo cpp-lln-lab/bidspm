c668cd090d05b8757b6b8dd1f531cfc3fe729ec5
Remi Gau <remi_gau@hotmail.com>
2021-03-10 11:17:14 +0100

    fix test

--- tests/test_unit_getFFXdir.m
+++ tests/test_unit_getFFXdir.m
@@ -1,6 +1,6 @@
 % (C) Copyright 2020 CPP BIDS SPM-pipeline developers
 
-function test_suite = test_getFFXdir %#ok<*STOUT>
+function test_suite = test_unit_getFFXdir %#ok<*STOUT>
   try % assignment of 'localfunctions' is necessary in Matlab >= 2016
     test_functions = localfunctions(); %#ok<*NASGU>
   catch % no problem; early Matlab versions can use initTestSuite fine
@@ -13,7 +13,7 @@ function test_getFFXdirBasic()
   funcFWFM = 0;
   subLabel = '01';
 
-  opt = setOptions('vislocalizer', subLabel);
+  opt = setOptions('funcLocalizer', subLabel);
   opt = setDerivativesDir(opt);
   opt = checkOptions(opt);
 
