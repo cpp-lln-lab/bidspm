f22ee28d4db31cae9402544d0b4428a045c27195
Michele MacLean <michele.maclean@umontreal.ca>
2021-03-15 15:43:49 -0400

    turn batch into a function

--- src/batches/setBatchSegmentationDetectLesion.m
+++ src/batches/setBatchSegmentationDetectLesion.m
@@ -1,16 +1,37 @@
-%-----------------------------------------------------------------------
-% Job saved on 08-Mar-2021 11:11:19 by cfg_util (rev $Rev: 7345 $)
-% spm SPM - SPM12 (7771)
-% cfg_basicio BasicIO - Unknown
-%-----------------------------------------------------------------------
-matlabbatch{1}.spm.tools.ali.unified_segmentation.step1data = '<UNDEFINED>';
+% (C) Copyright 2021 CPP BIDS SPM-pipeline developers
+
+function matlabbatch = setBatchSegmentationDetectLesion(matlabbatch, BIDS, opt, subID)
+  %
+  % Creates a batch to segment the anatomical image for lesion detection
+  %
+  % USAGE::
+  %
+  %   matlabbatch = setBatchSegmentationDetectLesion(matlabbatch, BIDS, opt, subID)
+  %
+  % :param matlabbatch: list of SPM batches
+  % :type matlabbatch: structure
+  %
+  % :returns: - :matlabbatch: (structure)
+
+  printBatchName('Segmentation for lesion detection');
+
+% find anatomical file
+[anatImage, anatDataDir] = getAnatFilename(BIDS, subID, opt);
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1data = [anatImage, anatDataDir];
+
+% define SPM folder 
 spmDir = spm('dir');
+
+% specify Prior EXTRA class (lesion prior map)
 lesionPriorMap = fullfile(spmDir, 'toolbox', 'ALI', 'Priors_extraClass', 'wc4prior0.nii');
+
 matlabbatch{1}.spm.tools.ali.unified_segmentation.step1prior = {lesionPriorMap};
-matlabbatch{1}.spm.tools.ali.unified_segmentation.step1niti = 2;
-matlabbatch{1}.spm.tools.ali.unified_segmentation.step1thr_prob = 0.333333333333333;
-matlabbatch{1}.spm.tools.ali.unified_segmentation.step1thr_size = 0.8;
-matlabbatch{1}.spm.tools.ali.unified_segmentation.step1coregister = 1;
-matlabbatch{1}.spm.tools.ali.unified_segmentation.step1mask = {''};
-matlabbatch{1}.spm.tools.ali.unified_segmentation.step1vox = 2;
-matlabbatch{1}.spm.tools.ali.unified_segmentation.step1fwhm = [8 8 8];
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1niti = 2;                       % number of iterations
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1thr_prob = 0.333333333333333;   % threshold probability
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1thr_size = 0.8;                 % threshold size (in cm3) 
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1coregister = 1;                  % coregister in MNI space (yes: 1)
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1mask = {''};                     % specify cost function mask CFM (optional)
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1vox = 2;                         % Voxel sizes (in mm)
+matlabbatch{1}.spm.tools.ali.unified_segmentation.step1fwhm = [8 8 8];                  % Smooth: FWHM
+
+end
