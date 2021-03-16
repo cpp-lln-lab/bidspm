Remi-Gau/CPP_BIDS_SPM_pipeline

first step to develop a ROI workflow

src/getInfo.m
@@ -40,9 +40,15 @@ function varargout = getInfo(BIDS, subID, opt, info, varargin)
 
     case 'sessions'
 
-      sessions = bids.query(BIDS, 'sessions', ...
-                            'sub', subID, ...
-                            'task', opt.taskName);
+      if isfield(opt, 'taskName')
+        sessions = bids.query(BIDS, 'sessions', ...
+                              'sub', subID, ...
+                              'task', opt.taskName);
+      else
+        sessions = bids.query(BIDS, 'sessions', ...
+                              'sub', subID);
+      end
+
       nbSessions = size(sessions, 2);
       if nbSessions == 0
         nbSessions = 1;

cpp-lln-lab/bidspm

src/getInfo.m
@@ -54,10 +54,13 @@ function varargout = getInfo(BIDS, subLabel, opt, info, varargin)
 
     case 'sessions'
 
-      query = struct( ...
-                     'sub',  subLabel, ...
-                     'task', opt.taskName);
-
+      if isfield(opt, 'taskName')
+        query = struct( ...
+                        'sub',  subLabel, ...
+                        'task', opt.taskName);
+      else
+        query = struct('sub',  subLabel);
+      end
       % upate query with pre-specified options
       % overwrite is set to true in this case because we might want to run
       % analysis only on certain sessions