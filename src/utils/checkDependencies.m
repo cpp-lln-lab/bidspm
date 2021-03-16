1021c8e8451dba1893e4aefaa1a6b2e4c49e41b3
Remi Gau <remi_gau@hotmail.com>
2021-03-16 22:00:14 +0100

    add a way to check that a toolbox is present

--- src/utils/checkDependencies.m
+++ src/utils/checkDependencies.m
@@ -1,33 +1,58 @@
 % (C) Copyright 2019 CPP BIDS SPM-pipeline developers
 
-function checkDependencies()
+function checkDependencies(varargin)
   %
   % Checks that that the right dependencies are installed:
   % - SPM
   % - Nifti tools
   % Also loads the spm defaults.
   %
+  % Can also be used to check the presence of SPM toolboxes like Marsbar
+  %
   % USAGE::
   %
   %   checkDependencies()
+  %
+  %   checkDependencies('marsbar')
   %
   % .. TODO:
   %
   %    - need to check other dependencies (bids-matlab, spmup)
   %
 
-  printCredits();
+  if ~isempty(varargin) && strcmpi(varargin{1}, 'marsbar')
+
+    toolbox = varargin{1};
+
+    present = false;
+
+    a = which('maroi_pointlist');
+    if ~isempty(a)
+      return
+    end
+    if ~present
+      foldersToCheck = { ...
+                        fullfile(spm('dir'), 'toolbox'); ...
+                        fullfile(fileparts(mfilename('fullpath')), '..', '..', 'lib')};
+
+      for iFolder = 1:numel(foldersToCheck)
+        toolbox_folder = spm_select('FPList', foldersToCheck{iFolder}, 'dir', ['^' toolbox '.*$']);
+        if ~isempty(toolbox_folder)
+          addpath(toolbox_folder);
+          fprintf(1, '\n\n%s added to path from folder:  %s\n\n', ...
+                  toolbox, foldersToCheck{iFolder});
+          return
+        end
+      end
+    end
+  end
+
+  fprintf('Checking dependencies\n');
 
   SPM_main = 'SPM12';
   SPM_sub = '7487';
 
-  nifti_tools_url = ...
-      ['https://www.mathworks.com/matlabcentral/fileexchange/' ...
-       '8797-tools-for-nifti-and-analyze-image'];
-
-  fprintf('Checking dependencies\n');
-
-  % check spm version
+  %% check spm version
   try
     [a, b] = spm('ver');
     fprintf(' Using %s %s\n', a, b);
@@ -40,9 +65,14 @@ function checkDependencies()
   catch
     error('Failed to check the SPM version: Are you sure that SPM is in the matlab path?');
   end
+
   spm('defaults', 'fmri');
 
-  % Check the Nifti tools are indeed there.
+  %% Check the Nifti tools are indeed there.
+  nifti_tools_url = ...
+      ['https://www.mathworks.com/matlabcentral/fileexchange/' ...
+       '8797-tools-for-nifti-and-analyze-image'];
+
   a = which('load_untouch_nii');
   if isempty(a)
     errorStruct.identifier = 'checkDependencies:missingDependency';
@@ -56,5 +86,7 @@ function checkDependencies()
   end
 
   fprintf(' We got all we need. Let''s get to work.\n');
+
+  printCredits();
 
 end
