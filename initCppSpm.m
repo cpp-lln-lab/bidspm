b936859b9553dc76edb2357e57fa12a0466c6ff1
Remi Gau <remi_gau@hotmail.com>
2021-03-19 11:25:34 +0100

    improve initCppSpm to run only once per matlab session

--- initCppSpm.m
+++ initCppSpm.m
@@ -1,21 +1,33 @@
 % (C) Copyright 2021 CPP BIDS SPM-pipeline developers
 
 function initCppSpm()
-
-  % directory with this script becomes the current directory
-  WD = fileparts(mfilename('fullpath'));
-
-  % we add all the subfunctions that are in the sub directories
-  addpath(genpath(fullfile(WD, 'src')));
-  addpath(genpath(fullfile(WD, 'lib', 'mancoreg')));
-  addpath(genpath(fullfile(WD, 'lib', 'NiftiTools')));
-  addpath(genpath(fullfile(WD, 'lib', 'spmup')));
-  addpath(genpath(fullfile(WD, 'lib', 'utils')));
-
-  addpath(fullfile(WD, 'lib', 'bids-matlab'));
-
-  checkDependencies();
-
-  printCredits();
-
+    
+    global CPP_SPM_INITIALIZED
+    
+    if isempty(CPP_SPM_INITIALIZED)
+        
+        % directory with this script becomes the current directory
+        thisDirectory = fileparts(mfilename('fullpath'));
+        
+        % we add all the subfunctions that are in the sub directories
+        addpath(genpath(fullfile(thisDirectory, 'src')));
+        addpath(genpath(fullfile(thisDirectory, 'lib', 'mancoreg')));
+        addpath(genpath(fullfile(thisDirectory, 'lib', 'NiftiTools')));
+        addpath(genpath(fullfile(thisDirectory, 'lib', 'spmup')));
+        addpath(genpath(fullfile(thisDirectory, 'lib', 'utils')));
+        
+        addpath(fullfile(thisDirectory, 'lib', 'bids-matlab'));
+        
+        checkDependencies();
+        
+        printCredits();
+        
+        CPP_SPM_INITIALIZED = true();
+    
+    else
+        fprintf('\n\nCPP_SPM already initialized\n\n');
+        
+    end
+    
+    
 end
