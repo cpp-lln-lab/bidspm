Remi-Gau/CPP_BIDS_SPM_pipeline

update initCppSpm

initCppSpm.m
@@ -11,7 +11,9 @@ function initCppSpm()
   addpath(genpath(fullfile(WD, 'lib', 'NiftiTools')));
   addpath(genpath(fullfile(WD, 'lib', 'spmup')));
   addpath(genpath(fullfile(WD, 'lib', 'utils')));
-
+    
+  addpath(fullfile(WD, 'lib', 'slice_display'));
+  addpath(fullfile(WD, 'lib', 'panel-2.14'));
   addpath(fullfile(WD, 'lib', 'bids-matlab'));
 
 end

cpp-lln-lab/bidspm

initCppSpm.m
@@ -17,6 +17,8 @@ function initCppSpm()
     addpath(genpath(fullfile(thisDirectory, 'lib', 'utils')));
 
     addpath(fullfile(thisDirectory, 'lib', 'bids-matlab'));
+    addpath(fullfile(thisDirectory, 'lib', 'slice_display'));
+    addpath(fullfile(thisDirectory, 'lib', 'panel-2.14'));
 
     checkDependencies();
 