500de904e7ce48991541a1d708102185b58f2aae
Remi Gau <remi_gau@hotmail.com>
2021-04-02 20:47:22 +0200

    update demo 114

--- demos/openneuro/ds000114_run.m
+++ demos/openneuro/ds000114_run.m
@@ -30,7 +30,30 @@ functionalQA(opt);
 
 bidsSmoothing(FWHM, opt);
 
+%% Run level analysis: as for MVPA
+
 bidsFFX('specifyAndEstimate', opt, FWHM);
+bidsFFX('contrasts', opt, FWHM);
+
+bidsConcatBetaTmaps(opt, FWHM, false, false);
+
+%% Subject level analysis: for regular univariate
+
+opt.model.file = fullfile(fileparts(mfilename('fullpath')), ...
+    'models', ...
+    'model-ds000114-linebisection_smdl.json');
+
+bidsFFX('specifyAndEstimate', opt, FWHM);
+
+opt.result.Steps(1) = struct( ...
+                           'Level',  'subject', ...
+                           'Contrasts', struct( ...
+                                               'Name', 'Correct_Task', ... % has to match
+                                               'Mask', false, ...
+                                               'MC', 'FWE', ... FWE, none, FDR
+                                               'p', 0.05, ...
+                                               'k', 0));
+
 bidsFFX('contrasts', opt, FWHM);
 bidsResults(opt, FWHM);
 
