8b7c65179932701d2763865498570762c225b826
Michele MacLean <michele.maclean@umontreal.ca>
2021-04-08 14:03:59 -0400

    modify lesion script and demo

--- demos/lesiondetection/batchLesion.m
+++ demos/lesiondetection/batchLesion.m
@@ -3,12 +3,20 @@
 clear;
 clc;
 
+% This script will download the CVI dataset and will run 1) lesion segmentation,
+% 2)lesion abnormalities detection and 3)lesion overlapmap adapted from the ALI
+% toolbox (SPM.)
+
 % URL of the data set to download
 % URL = https://gin.g-node.org/mwmaclean/CVI-Datalad/src/master/data
 
+
+%downloadData = false;
+
 run ../../initCppSpm.m;
 
-%% Get Data
+
+%% Set options
 opt = Lesion_getOption();
 
 %% Run batches
@@ -17,4 +25,17 @@ reportBIDS(opt);
 deleteZippedNii = true;
 bidsCopyRawFolder(opt, deleteZippedNii, {'anat'});
 
+% Step 1: segmentation
 bidsLesionSegmentation(opt);
+
+% % Step 2: lesion abnormalities
+% bidsLesionAbnormalitiesDetection(opt)
+% 
+% % Step 3: overlap map
+% bidsLesionOverlapMap(opt)
+
+
+%% % Get data
+
+
+%To do. add function data download
