1a4467877f9c6ebe5e8c076148c3607788622160
Ceren <battal.ceren@gmail.com>
2021-04-12 16:46:17 +0200

    add make mean functional image mask

---
+++ src/subject_level/createFuncWholeBrainMask.m
@@ -0,0 +1,35 @@
+% (C) Copyright 2020 RnB FFT-analysis developers
+
+function mask = createFuncWholeBrainMask(opt)
+
+    % create segmented-skull stripped mean functional image
+    
+    % read the dataset
+    [~, opt, BIDS] = getData(opt);
+    subLabel = opt.subjects{1};
+    
+    % call/create the mask name
+    [meanImage, meanFuncDir] = getMeanFuncFilename(BIDS, subLabel, opt);
+    
+    % name the output accordingto the input image
+    maskFileName = ['m' strrep(meanImage, '.nii', '_mask.nii')];
+    mask = fullfile(meanFuncDir, maskFileName);
+
+    % ask if mask exist, if not create it:
+    if ~exist(mask,'file')
+        
+        % set batch order since there is dependencies
+        opt.orderBatches.segment = 1;
+        opt.orderBatches.skullStripping = 2;
+
+        % opt for running skull strip on the mean image
+        opt.skullStripMeanImg = 1;
+
+        % make matlab batch for segment and skullstip
+        matlabbatch = [];
+        matlabbatch = setBatchSegmentation(matlabbatch, opt, opt.funcMaskFileName);
+
+        matlabbatch = setBatchSkullStripping(matlabbatch, BIDS, opt, subLabel);
+        % run spm
+        saveAndRunWorkflow(matlabbatch, 'meanImage_segment_skullstrip', opt, subLabel);
+    end
