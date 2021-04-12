a917b13eff01b7dd7cffb5f75b41bbbc56667956
Michele MacLean <michele.maclean@umontreal.ca>
2021-04-12 09:55:41 -0400

    remove global variable

--- src/defaults/ALI_my_defaults.m
+++ src/defaults/ALI_my_defaults.m
@@ -17,7 +17,6 @@ function ALI_my_defaults()
   % back on the defaults to fill in the blanks. This allows to make the
   % script simpler.
   
-  global defaults
   
     % Lesion segmentation defaults
   % ==========================================================================
@@ -28,38 +27,36 @@ function ALI_my_defaults()
 %   % specify Prior EXTRA class (lesion prior map)
 %   lesionPriorMap = fullfile(spmDir, 'toolbox', 'ALI', 'Priors_extraClass', 'wc4prior0.nii');
 
-  defaults.unified_segmentation.step1prior = {lesionPriorMap};
+  defaults.toolbox.ALI.unified_segmentation.step1prior = {lesionPriorMap};
 
-  defaults.unified_segmentation.step1niti = 2;                     % number of iterations
-  defaults.unified_segmentation.step1thr_prob = 0.333333333333333; % threshold probability
-  defaults.unified_segmentation.step1thr_size = 0.8;               % threshold size (in cm3)
-  defaults.unified_segmentation.step1coregister = 1;               % coregister in MNI space (yes: 1)
-  defaults.unified_segmentation.step1mask = {''};                  % specify cost function mask(optional)
-  defaults.unified_segmentation.step1vox = 2;                      % Voxel sizes (in mm)
-  defaults.unified_segmentation.step1fwhm = [8 8 8];               % Smooth: FWHM
+  defaults.toolbox.ALI.unified_segmentation.step1niti = 2;                     % number of iterations
+  defaults.toolbox.ALI.unified_segmentation.step1thr_prob = 0.333333333333333; % threshold probability
+  defaults.toolbox.ALI.unified_segmentation.step1thr_size = 0.8;               % threshold size (in cm3)
+  defaults.toolbox.ALI.unified_segmentation.step1coregister = 1;               % coregister in MNI space (yes: 1)
+  defaults.toolbox.ALI.unified_segmentation.step1mask = {''};                  % specify cost function mask(optional)
+  defaults.toolbox.ALI.unified_segmentation.step1vox = 2;                      % Voxel sizes (in mm)
+  defaults.toolbox.ALI.unified_segmentation.step1fwhm = [8 8 8];               % Smooth: FWHM
 
 
     % Lesion abnormalities detection defaults
   % ==========================================================================
   
   % Specify alpha parameter
-  defaults.matlabbatch{1}.spm.tools.ali.outliers_detection.step3tissue.step3Alpha = 0.5;
+  defaults.toolbox.ALI.matlabbatch{1}.spm.tools.ali.outliers_detection.step3tissue.step3Alpha = 0.5;
   % Specify lambda parameter
-  defaults.matlabbatch{1}.spm.tools.ali.outliers_detection.step3tissue.step3Lambda = -4;
+  defaults.toolbox.ALI.matlabbatch{1}.spm.tools.ali.outliers_detection.step3tissue.step3Lambda = -4;
 
 %   % define SPM folder
 %   spmDir = spm('dir');
 
   % specify lesion mask
-  defaults.lesionMask = fullfile(spmDir, 'toolbox', 'ALI', 'Mask_image', 'mask_controls_vox2mm.nii');
+  defaults.toolbox.ALI.lesionMask = fullfile(spmDir, 'toolbox', 'ALI', 'Mask_image', 'mask_controls_vox2mm.nii');
 
-  defaults.outliers_detection.step3mask_thr = 0;           % threshold for the mask
-  defaults.outliers_detection.step3binary_thr = 0.3;       % binary lesion: threshold U
-  defaults.outliers_detection.step3binary_size = 0.8;      % binary lesion: minimum size (in cm3)
+  defaults.toolbox.ALI.outliers_detection.step3mask_thr = 0;           % threshold for the mask
+  defaults.toolbox.ALI.outliers_detection.step3binary_thr = 0.3;       % binary lesion: threshold U
+  defaults.toolbox.ALI.outliers_detection.step3binary_size = 0.8;      % binary lesion: minimum size (in cm3)
 %   
 
-% No apparent defaults for step3 overlap map
-  
 end
   
   
