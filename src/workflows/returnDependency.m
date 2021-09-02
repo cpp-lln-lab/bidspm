e1df8406b24195a92612aa9eb21d4a1c4da33353
Remi Gau <remi_gau@hotmail.com>
2021-09-02 14:26:11 +0200

    [RF] extract function returnDependency

---
+++ src/workflows/returnDependency.m
@@ -0,0 +1,26 @@
+function dep = returnDependency(opt, type)
+  %
+  % USAGE::
+  %
+  %  dep = returnDependency(opt, type)
+  %
+  % (C) Copyright 2021 CPP_SPM developers
+
+  switch type
+
+    case {'segment', 'skullStripping'}
+      dep = substruct('.', 'val', '{}', {opt.orderBatches.(type)}, ...
+                      '.', 'val', '{}', {1}, ...
+                      '.', 'val', '{}', {1});
+
+    case {'coregister'}
+      dep = substruct('.', 'val', '{}', {opt.orderBatches.(type)}, ...
+                      '.', 'val', '{}', {1}, ...
+                      '.', 'val', '{}', {1}, ...
+                      '.', 'val', '{}', {1});
+    otherwise
+      dep = '';
+
+  end
+
+end
