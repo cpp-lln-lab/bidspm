93e1bc998b5e0ade3d71ac6f8777bcaf4f05ce63
Remi Gau <remi_gau@hotmail.com>
2021-09-04 15:33:24 +0200

    add first draft GLM wioth fmriprep

---
+++ demos/MoAE/moae_fmriprep_stats.m
@@ -0,0 +1,27 @@
+% This script will run the FFX and contrasts on it of the MoAE dataset
+%
+% Results might be a bit different from those in the manual as some
+% default options are slightly different in this pipeline
+% (e.g use of FAST instead of AR(1), motion regressors added)
+%
+% (C) Copyright 2019 Remi Gau
+
+clear;
+clc;
+
+run ../../initCppSpm.m;
+
+% download_data = true;
+% download_moae_ds(download_data);
+%
+% opt = moae_get_option_fmriprep_preproc();
+%
+% bidsCopyInputFolder(opt);
+%
+% bidsSmoothing(opt);
+
+opt = moae_get_option_fmriprep_stats();
+
+bidsFFX('specifyAndEstimate', opt);
+bidsFFX('contrasts', opt);
+bidsResults(opt);
---
+++ demos/MoAE/moae_get_option_fmriprep_preproc.m
@@ -0,0 +1,26 @@
+function opt = moae_get_option_fmriprep_preproc()
+  %
+  % returns a structure that contains the options chosen by the user to run
+  % slice timing correction, pre-processing, FFX, RFX.
+  %
+  % (C) Copyright 2019 Remi Gau
+
+  opt = [];
+
+  % task to analyze
+  opt.taskName = 'auditory';
+  opt.verbosity = 1;
+
+  % The directory where the data are located
+  WD = fileparts(mfilename('fullpath'));
+  opt.dir.raw = fullfile(WD, 'inputs', 'raw');
+  opt.dir.input = fullfile(WD, 'spm_moae_fmriprep');
+  opt.dir.derivatives = fullfile(WD, 'outputs', 'derivatives');
+
+  opt.space = {'MNI152NLin2009cAsym'};
+
+  %% DO NOT TOUCH
+  opt = checkOptions(opt);
+  saveOptions(opt);
+
+end
