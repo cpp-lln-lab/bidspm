08ac263c61051465b7548a8f4c7984cf58407c0f
Remi Gau <remi_gau@hotmail.com>
2021-09-09 17:41:26 +0200

    add draft calls to get reference and source images

--- src/workflows/bidsMidbrainReslice.m
+++ src/workflows/bidsMidbrainReslice.m
@@ -5,11 +5,21 @@ function bidsMidbrainReslice(opt)
   % bidsResliceMidbrain(opt)
   % Relice midbrain masks at the correct resolution
 
+  opt.dir.input = opt.dir.preproc;
+
   [BIDS, opt] = setUpWorkflow(opt, 'Midbrain Reslice');
+
+  use_schema = false;
+  BIDSroi =  bids.layout(opt.dir.roi, use_schema);
 
   for iSub = 1:numel(opt.subjects)
 
     subLabel = opt.subjects{iSub};
+
+    roiFiles = bids.query(BIDSroi, 'data', 'sub', subLabel);
+    roiFiles = bids.query(BIDSroi, 'data', 'sub', '01', 'label', 'V1d', 'hemi', 'R');
+
+    bids.query(BIDS, 'data', 'space', 'individual', 'desc', 'mean', 'suffix', 'bold');
 
     printProcessingSubject(iSub, subLabel);
 
