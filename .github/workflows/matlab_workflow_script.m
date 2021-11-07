6e2711f56aff2d5647d69d337ec5bae4f3b0fe66
Remi Gau <remi_gau@hotmail.com>
2021-11-07 19:00:31 +0100

    add tests subfun to path

--- .github/workflows/matlab_workflow_script.m
+++ .github/workflows/matlab_workflow_script.m
@@ -1,12 +1,9 @@
 
 root_dir = getenv('GITHUB_WORKSPACE');
 
-disp(root_dir)
-
-ls
-
 addpath(fullfile(root_dir, 'spm12'));
 addpath(fullfile(root_dir, 'MOcov', 'MOcov'));
+addpath(fullfile(root_dir, 'tests', 'utils'));
 
 cd(fullfile(root_dir, 'MOxUnit', 'MOxUnit'));
 run moxunit_set_path(); 
