61497c030bb4609ff14250bcdeb67744447c503c
Remi Gau <remi_gau@hotmail.com>
2021-12-20 15:18:15 +0100

    fix typo on print figure batch

--- src/batches/setBatchPrintFigure.m
+++ src/batches/setBatchPrintFigure.m
@@ -20,8 +20,8 @@ function matlabbatch = setBatchPrintFigure(matlabbatch, opt, figureName)
     printBatchName('print figure', opt);
 
     matlabbatch{end + 1}.spm.util.print.fname = figureName;
-    matlabbatch{1}.spm.util.print.fig.figname = 'Graphics';
-    matlabbatch{1}.spm.util.print.opts = 'png';
+    matlabbatch{end}.spm.util.print.fig.figname = 'Graphics';
+    matlabbatch{end}.spm.util.print.opts = 'png';
 
   end
 
