ab14b7c46bb18302995f3b6162a72351449884b6
Remi Gau <remi_gau@hotmail.com>
2021-12-22 17:56:10 +0100

    refactor elapsedTme and use verbosity for it.

--- src/workflows/cleanUpWorkflow.m
+++ src/workflows/cleanUpWorkflow.m
@@ -1,11 +1,11 @@
 function cleanUpWorkflow(opt)
-    %
-    % USAGE::
-    %
-    %   cleanUpWorkflow(opt)
-    %
-    % (C) Copyright 2021 CPP_SPM developers
+  %
+  % USAGE::
+  %
+  %   cleanUpWorkflow(opt)
+  %
+  % (C) Copyright 2021 CPP_SPM developers
 
-    elapsedTime('globalStop', opt.globalStart);
+  elapsedTime(opt, 'globalStop');
 
 end
---
+++ tests/tests_utils/test_elapsedTime.m
@@ -0,0 +1,33 @@
+function test_suite = test_elapsedTime %#ok<*STOUT>
+  %
+  % (C) Copyright 2020 CPP_SPM developers
+
+  try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+    test_functions = localfunctions(); %#ok<*NASGU>
+  catch % no problem; early Matlab versions can use initTestSuite fine
+  end
+  initTestSuite;
+end
+
+function test_elapsedTime_basic()
+
+  opt = setOptions('vislocalizer');
+
+  [~, opt] = setUpWorkflow(opt, 'test elapsed time');
+
+  nbIteration = numel(opt.subjects);
+  runTime = [];
+
+  for iSub = 1:nbIteration
+    subLabel = opt.subjects{iSub};
+    printProcessingSubject(iSub, subLabel, opt);
+    subjectStart = elapsedTime(opt, 'start');
+    pause(1);
+    [~, runTime] = elapsedTime(opt, 'stop', subjectStart, runTime, nbIteration);
+  end
+
+  pause(1);
+
+  cleanUpWorkflow(opt);
+
+end
