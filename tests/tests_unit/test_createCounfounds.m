88d666a4156b78e7c4ba326a70d2c3509c97f8fc
Remi Gau <remi_gau@hotmail.com>
2021-12-28 07:57:38 +0100

    improve confound regressors creations to support globbing

---
+++ tests/tests_unit/test_createCounfounds.m
@@ -0,0 +1,115 @@
+% (C) Copyright 2021 CPP_SPM developers
+
+function test_suite = test_createCounfounds %#ok<*STOUT>
+  try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+    test_functions = localfunctions(); %#ok<*NASGU>
+  catch % no problem; early Matlab versions can use initTestSuite fine
+  end
+  initTestSuite;
+end
+
+function test_createAndReturnCounfoundMatFile_maxNbVols()
+
+  tsvFile = fullfile(getDummyDataDir, 'sub-01_task-test_desc-confounds_regressors.tsv');
+  tsvContent = bids.util.tsvread(tsvFile);
+
+  designMatrix = { 'trial_type.VisMot'
+                  'trial_type.VisStat'
+                  'trial_type.missing_condition'
+                  'trans_x'
+                  'trans_y'
+                  'trans_z'
+                  'rot_x'
+                  'rot_y'
+                  'rot_z'};
+
+  [~, R] = createConfounds(tsvContent, designMatrix, 200);
+
+  assertEqual(size(R, 1), 200);
+
+end
+
+function test_createAndReturnCounfoundMatFile_maxNbVols_gt_actualNbVols()
+
+  tsvFile = fullfile(getDummyDataDir, 'sub-01_task-test_desc-confounds_regressors.tsv');
+  tsvContent = bids.util.tsvread(tsvFile);
+
+  designMatrix = { 'trial_type.VisMot'
+                  'trial_type.VisStat'
+                  'trial_type.missing_condition'
+                  'trans_x'
+                  'trans_y'
+                  'trans_z'
+                  'rot_x'
+                  'rot_y'
+                  'rot_z'};
+
+  [~, R] = createConfounds(tsvContent, designMatrix, 400);
+
+  assertEqual(size(R, 1), 283);
+
+end
+
+function test_createAndReturnCounfoundMatFile_outlier_regressors()
+
+  tsvFile = fullfile(getDummyDataDir, 'sub-01_task-test_desc-confounds_regressors.tsv');
+  tsvContent = bids.util.tsvread(tsvFile);
+
+  designMatrix = { 'trial_type.VisMot'
+                  'trial_type.VisStat'
+                  'trial_type.missing_condition'
+                  'rot_x'
+                  'rot_y'
+                  'rot_z'
+                  'trans_x'
+                  'trans_y'
+                  'trans_z'
+                  'non_steady_state_outlier*'
+                  'motion_outlier*'};
+
+  names = createConfounds(tsvContent, designMatrix, Inf);
+
+  expectedNames = {        'rot_x'
+                   'rot_y'
+                   'rot_z'
+                   'trans_x'
+                   'trans_y'
+                   'trans_z'
+                   'non_steady_state_outlier00'
+                   'non_steady_state_outlier01'
+                   'non_steady_state_outlier02'
+                   'motion_outlier00'
+                   'motion_outlier01'};
+
+  assertEqual(names, expectedNames);
+end
+
+function test_createAndReturnCounfoundMatFile_regex()
+
+  tsvFile = fullfile(getDummyDataDir, 'sub-01_task-test_desc-confounds_regressors.tsv');
+  tsvContent = bids.util.tsvread(tsvFile);
+
+  designMatrix = { 'trial_type.VisMot'
+                  'trial_type.VisStat'
+                  'trial_type.missing_condition'
+                  'trans_?'
+                  'rot_?'
+                  'non_steady_state_outlier*'
+                  'motion_outlier*'};
+
+  names = createConfounds(tsvContent, designMatrix, Inf);
+
+  expectedNames = {                   'trans_x'
+                   'trans_y'
+                   'trans_z'
+                   'rot_x'
+                   'rot_y'
+                   'rot_z'
+                   'non_steady_state_outlier00'
+                   'non_steady_state_outlier01'
+                   'non_steady_state_outlier02'
+                   'motion_outlier00'
+                   'motion_outlier01'};
+
+  assertEqual(names, expectedNames);
+end
