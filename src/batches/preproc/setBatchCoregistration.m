4067ab15200e3c71339af2809b22d4fcc6b87c30
Remi Gau <remi_gau@hotmail.com>
2022-01-05 13:03:39 +0100

    use argparse for setBatchCoregistration

--- src/batches/preproc/setBatchCoregistration.m
+++ src/batches/preproc/setBatchCoregistration.m
@@ -1,4 +1,4 @@
-function matlabbatch = setBatchCoregistration(matlabbatch, opt, ref, src, other)
+function matlabbatch = setBatchCoregistration(varargin)
   %
   % Set the batch for coregistering the source images into the reference image
   %
@@ -12,14 +12,30 @@ function matlabbatch = setBatchCoregistration(matlabbatch, opt, ref, src, other)
   % :type ref: string
   % :param src: Source image
   % :type src: string
-  % :param other: ?
+  % :param other: Other images to apply the coregistration to
   % :type other: cell string
   %
   % :returns: - :matlabbatch: (structure) The matlabbatch ready to run the spm job
   %
-  % matlabbatch = setBatchCoregistrationGeneral(matlabbatch, ref, src, other)
-  %
   % (C) Copyright 2020 CPP_SPM developers
+
+  default_other = {''};
+
+  isFile = @(x) exist(x, 'file')==2;
+
+  p = inputParser;
+
+  addRequired(p, 'matlabbatch', @iscell);
+  addRequired(p, 'ref', isFile);
+  addRequired(p, 'src', isFile);
+  addOptional(p, 'other', default_other, @iscell);
+
+  parse(p, varargin{:});
+
+  matlabbatch = p.Results.matlabbatch;
+  ref = p.Results.ref;
+  src = p.Results.src;
+  other = p.Results.other;
 
   printBatchName('coregistration', opt);
 
