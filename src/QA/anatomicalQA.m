287d25c09f55a1ab73a788e3497ddd2416b293a9
Remi Gau <remi_gau@hotmail.com>
2022-01-10 17:42:41 +0100

    remove extracted subfunction

--- src/QA/anatomicalQA.m
+++ src/QA/anatomicalQA.m
@@ -73,31 +73,3 @@ function anatomicalQA(opt)
   end
 
 end
-
-function davg = getDist2surf(anatImage, opt)
-
-  davg = 50;
-
-  path = spm_fileparts(anatImage);
-
-  bf = bids.File(anatImage);
-
-  surface_file = spm_select('FPList', path, ['^.*sub-' bf.entities.sub '.*_' bf.suffix '\.gii$']);
-
-  if ~isempty(surface_file)
-
-    FV = gifti(surface_file);
-    center = FV.vertices(FV.faces(:, :), :);
-    center = reshape(center, [size(FV.faces, 1) 3 3]);
-    center = squeeze(mean(center, 2));
-    ori_dist = sqrt(sum((center .* -1).^2, 2))';
-    davg = mean(ori_dist); %#ok<UDIM>
-
-  else
-
-    printToScreen(' Could not compute the average distance to the brain surface.\n', opt);
-    printToScreen(' Using the default value instead.\n', opt);
-
-  end
-
-end
