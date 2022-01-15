2f649e55b61910314fd977102a252367b8d6b662
Remi Gau <remi_gau@hotmail.com>
2022-01-15 15:31:21 +0100

    add coverage for errors

--- src/utils/getROIs.m
+++ src/utils/getROIs.m
@@ -1,7 +1,7 @@
 function [roiList, roiFolder] = getROIs(varargin)
   %
   % get the rois from :
-  % 
+  %
   % - the group folder when running analysis in MNI space
   % - the sub-*/roi/ folder when in individual space
   %
@@ -11,44 +11,97 @@ function [roiList, roiFolder] = getROIs(varargin)
   %
   %
   % (C) Copyright 2021 CPP_SPM developers
-  
+
   p = inputParser;
+
   default_subLabel = '';
   charOrCell = @(x) ischar(x) || iscell(x);
+
   addRequired(p, 'opt');
   addOptional(p, 'subLabel', default_subLabel, charOrCell);
+
   parse(p, varargin{:});
+
   opt = p.Results.opt;
   subLabel = p.Results.subLabel;
 
+  % outputs
   roiList = {};
   roiFolder = '';
 
-  if strcmp(subLabel, '') || ...
-      any(~cellfun('isempty', regexp(opt.space, 'MNI'))) || ...
-      ismember('IXI549Space', opt.space)
-    
+  % in case user asked for specific ROIs
+  roiNames = getRoiNames(opt);
+  % We assume that the "label" of the roi filenames is requested in roiNames.
+  % We return here to keep the behavior consistent with bids.query
+  % that filters an entity out when asking for an empty entity.
+  if strcmp(roiNames, '')
+    return
+  end
+
+  % Identify if we are in MNI or individual space
+  space = getSpace(opt);
+
+  if strcmp(space, 'MNI')
+
     % here we don't expect BIDS name except from having a 'mask' suffix
     roiFolder = fullfile(opt.dir.roi, 'group');
-    roiList = spm_select('FPlist', roiFolder, '^.*_mask.nii$');
+
+    pattern = '';
+    if ~isempty(roiNames)
+      pattern = ['(' strjoin(roiNames, '|') '){1}'];
+    end
+    roiList = spm_select('FPlist', roiFolder, ['^.*' pattern '.*_mask.nii$']);
     roiList = cellstr(roiList);
 
-  elseif strcmp(opt.space, 'individual')
-    
+  elseif strcmp(space, 'individual')
+
     % we expect ROI files to have BIDS valid names
     use_schema = false;
     BIDS_ROI = bids.layout(opt.dir.roi, use_schema);
-    
-    roiFolder = fullfile(BIDS_ROI.pth, ['sub-' subLabel], 'roi');
-    
-    roiList = bids.query(BIDS_ROI, 'data', ...
-                         'sub', regexify(subLabel), ...
-                         'space', opt.space);
-    
-  else
 
-    msg = sprintf('unknwon space:\n%s', createUnorderedList(opt.space));
-    errorHandling(mfilename(), 'unknownSpace', msg, true, opt.verbosity > 0);
+    if strcmp(subLabel, '')
+      msg = sprintf('Provide a subject label amongst those:\n%s\n\n', ...
+                    createUnorderedList(bids.query(BIDS_ROI, 'subjects')));
+      errorHandling(mfilename(), 'noSubject', msg, false, opt.verbosity > 0);
+    end
+
+    roiFolder = fullfile(BIDS_ROI.pth, ['sub-' subLabel], 'roi');
+
+    filter = struct('sub', regexify(subLabel), ...
+                    'space', opt.space);
+
+    if ~isempty(roiNames)
+      if iscell(roiNames) && (numel(roiNames) == 1 && ~strcmp(roiNames{1}, ''))
+        filter.label = ['(' strjoin(opt.roi.name, '|') '){1}'];
+      end
+    end
+
+    roiList = bids.query(BIDS_ROI, 'data', filter);
+
+  end
+
+end
+
+function roiNames = getRoiNames(opt)
+  roiNames = [];
+  if isfield(opt, 'roi') && isfield(opt.roi, 'name')
+    roiNames = opt.roi.name;
+  end
+end
+
+function space = getSpace(opt)
+
+  space = opt.space;
+
+  if any(~cellfun('isempty', regexp(space, 'MNI'))) || ismember('IXI549Space', space)
+    space = 'MNI';
+
+  elseif strcmp(opt.space, 'individual')
+    space = 'individual';
+
+  else
+    msg = sprintf('unknwon space:\n%s', createUnorderedList(space));
+    errorHandling(mfilename(), 'unknownSpace', msg, false, opt.verbosity > 0);
 
   end
 
--- tests/tests_utils/test_getROIs.m
+++ tests/tests_utils/test_getROIs.m
@@ -11,6 +11,50 @@ function test_suite = test_getROIs %#ok<*STOUT>
 
 end
 
+function test_getROIs_unknown_space()
+
+  opt = setTestCfg();
+  opt.dir.roi = getDummyDataDir('roi');
+  opt.space = 'foo';
+  opt = checkOptions(opt);
+
+  assertExceptionThrown(@()getROIs(opt), 'getROIs:unknownSpace');
+
+end
+
+function test_getROIs_individual_space_no_subject()
+
+  opt = setTestCfg();
+  opt.dir.roi = getDummyDataDir('roi');
+  opt.space = 'individual';
+  opt = checkOptions(opt);
+
+  assertExceptionThrown(@()getROIs(opt), 'getROIs:noSubject');
+
+end
+
+function test_getROIs_no_roi()
+
+  opt = setTestCfg();
+  opt.dir.roi = getDummyDataDir('roi');
+  opt = checkOptions(opt);
+
+  opt.roi.name = {''};
+
+  opt.space = 'MNI';
+  [roiList, roiFolder] = getROIs(opt);
+
+  assertEqual(roiFolder, '');
+  assertEqual(roiList, {});
+
+  opt.space = 'individual';
+  [roiList, roiFolder] = getROIs(opt);
+
+  assertEqual(roiFolder, '');
+  assertEqual(roiList, {});
+
+end
+
 function test_getROIs_mni()
 
   opt = setTestCfg();
@@ -35,15 +79,32 @@ function test_getROIs_mni_subselect()
   opt.dir.roi = getDummyDataDir('roi');
   opt.space = 'MNI';
   opt = checkOptions(opt);
+
   opt.roi.name = {'V1'};
 
   [roiList, roiFolder] = getROIs(opt);
 
-  expectedFolder = fullfile(opt.dir.roi, 'group');
-  assertEqual(roiFolder, expectedFolder);
-
   expectedRoiLists = {fullfile(roiFolder, 'V1_mask.nii')};
   assertEqual(roiList, expectedRoiLists);
+
+  % test regex
+  opt.roi.name = {'A|V'};
+  roiList = getROIs(opt);
+  expectedRoiLists = {fullfile(roiFolder, 'A1_mask.nii'); ...
+                      fullfile(roiFolder, 'V1_mask.nii')};
+  assertEqual(roiList, expectedRoiLists);
+
+  opt.roi.name = {'.*1'};
+  roiList = getROIs(opt);
+  assertEqual(roiList, expectedRoiLists);
+
+  opt.roi.name = {'A1', 'V1'};
+  roiList = getROIs(opt);
+  assertEqual(roiList, expectedRoiLists);
+
+  opt.roi.name = {'foo'};
+  roiList = getROIs(opt);
+  assertEqual(roiList, {''});
 
 end
 
@@ -64,6 +125,49 @@ function test_getROIs_individual()
                       fullfile(roiFolder, 'sub-01_hemi-L_space-individual_label-V1_mask.nii'); ...
                       fullfile(roiFolder, 'sub-01_hemi-R_space-individual_label-A1_mask.nii'); ...
                       fullfile(roiFolder, 'sub-01_hemi-R_space-individual_label-V1_mask.nii')};
+  assertEqual(roiList, expectedRoiLists);
+
+end
+
+function test_getROIs_individual_subselect()
+
+  opt = setTestCfg();
+  subLabel = '01';
+  opt.dir.roi = getDummyDataDir('roi');
+  opt.space = 'individual';
+  opt = checkOptions(opt);
+
+  opt.roi.name = {'V1'};
+
+  [roiList, roiFolder] = getROIs(opt, subLabel);
+
+  expectedRoiLists = {fullfile(roiFolder, 'sub-01_hemi-L_space-individual_label-V1_mask.nii'); ...
+                      fullfile(roiFolder, 'sub-01_hemi-R_space-individual_label-V1_mask.nii')};
+  assertEqual(roiList, expectedRoiLists);
+
+  % test regex
+
+  opt.roi.name = {'.*1'};
+  roiList = getROIs(opt, subLabel);
+  expectedRoiLists = {fullfile(roiFolder, 'sub-01_hemi-L_space-individual_label-A1_mask.nii'); ...
+                      fullfile(roiFolder, 'sub-01_hemi-L_space-individual_label-V1_mask.nii'); ...
+                      fullfile(roiFolder, 'sub-01_hemi-R_space-individual_label-A1_mask.nii'); ...
+                      fullfile(roiFolder, 'sub-01_hemi-R_space-individual_label-V1_mask.nii')};
+  assertEqual(roiList, expectedRoiLists);
+
+  opt.roi.name = {'A1', 'V1'};
+  roiList = getROIs(opt, subLabel);
+  assertEqual(roiList, expectedRoiLists);
+
+  opt.roi.name = {'foo'};
+  roiList = getROIs(opt, subLabel);
+  assertEqual(roiList, {});
+
+  % TODO: check why this regex fails in bids matlab
+  %   opt.roi.name = {'A|V'};
+
+  opt.roi.name = {'A', 'V'};
+  roiList = getROIs(opt, subLabel);
   assertEqual(roiList, expectedRoiLists);
 
 end
