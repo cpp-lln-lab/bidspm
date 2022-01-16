1a598dc3dba647ddbc5e5672a3728fbdb100ea5b
Remi Gau <remi_gau@hotmail.com>
2022-01-16 13:53:34 +0100

    add matlab GH action workflow

---
+++ .github/workflows/matlab_workflow_script.m
@@ -0,0 +1,14 @@
+%
+% (C) Copyright 2021 CPP_SPM developers
+
+root_dir = getenv('GITHUB_WORKSPACE');
+
+addpath(fullfile(root_dir, 'spm12'));
+addpath(fullfile(root_dir, 'MOcov', 'MOcov'));
+
+cd(fullfile(root_dir, 'MOxUnit', 'MOxUnit'));
+run moxunit_set_path();
+
+cd(fullfile(root_dir));
+initCppSpm(true);
+run run_tests();
