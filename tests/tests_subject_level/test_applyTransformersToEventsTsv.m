db59fb7bb8f1c07eaf1be5012193ded31edbf7ba
Remi Gau <remi_gau@hotmail.com>
2022-01-27 00:25:29 +0100

    update CPP SPM code to deal with offsetting several variables

--- tests/tests_subject_level/test_applyTransformersToEventsTsv.m
+++ tests/tests_subject_level/test_applyTransformersToEventsTsv.m
@@ -1,61 +1,82 @@
 function test_suite = test_applyTransformersToEventsTsv %#ok<*STOUT>
   %
   % (C) Copyright 2022 CPP_SPM developers
-
+  
   try % assignment of 'localfunctions' is necessary in Matlab >= 2016
     test_functions = localfunctions(); %#ok<*NASGU>
   catch % no problem; early Matlab versions can use initTestSuite fine
   end
-
+  
   initTestSuite;
+  
+end
 
+function test_applyTransformersToEventsTsv_several_inputs_outputs()
+  
+  % GIVEN
+  tsvFile = fullfile(getDummyDataDir(), 'sub-01_task-vismotion_events.tsv');
+  tsvContent = bids.util.tsvread(tsvFile);
+  
+  transformers = struct('Name', 'Subtract', ...
+    'Input', {{'trial_type.VisMot', 'trial_type.VisStat'}}, ...
+    'Value', 3, ...
+    'Output', {{'VisMot', 'VisStat'}});
+  
+  % WHEN
+  newContent = applyTransformersToEventsTsv(tsvContent, transformers);
+  
+  % TH
+  assertEqual(fieldnames(newContent), {'VisMot'; 'VisStat'});
+  
+  cleanUp();
+  
 end
 
 function test_applyTransformersToEventsTsv_no_transformation()
-
+  
   % GIVEN
   tsvFile = fullfile(getDummyDataDir(), 'sub-01_task-vismotion_events.tsv');
   tsvContent = bids.util.tsvread(tsvFile);
-
+  
   opt = setOptions('vislocalizer');
   transformers = getBidsTransformers(opt.model.file);
-
+  
   % WHEN
   newContent = applyTransformersToEventsTsv(tsvContent, transformers);
-
+  
   % THEN
   assertEqual(newContent, struct([]));
-
+  
   cleanUp();
-
+  
 end
 
 function test_applyTransformersToEventsTsv_basic
-
+  
   % GIVEN
   tsvFile = fullfile(getDummyDataDir(), 'sub-01_task-vismotion_events.tsv');
   tsvContent = bids.util.tsvread(tsvFile);
-
+  
   opt.model.file = fullfile(getDummyDataDir(),  'models', ...
-                            'model-vismotionWithTransformation_smdl.json');
+    'model-vismotionWithTransformation_smdl.json');
   transformers = getBidsTransformers(opt.model.file);
-
+  
   % WHEN
   newContent = applyTransformersToEventsTsv(tsvContent, transformers);
-
+  
   % THEN
-  assertEqual({'VisMot'; 'VisStat'}, fieldnames(newContent));
+  assertEqual(fieldnames(newContent), {'VisMot'; 'VisStat'});
   assertEqual(newContent.VisMot, struct('onset', -1, 'duration', 2));
   assertEqual(newContent.VisStat, struct('onset', 5, 'duration', 2));
-
+  
   cleanUp();
-
+  
 end
 
 function setUp()
-
+  
 end
 
 function cleanUp()
-
+  
 end
