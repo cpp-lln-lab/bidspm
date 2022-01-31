dca497b953579b53dcdb173b8c60e260c1a2f440
Remi Gau <remi_gau@hotmail.com>
2022-01-31 15:55:42 +0100

    fix returnRootDir

--- src/infra/returnRootDir.m
+++ src/infra/returnRootDir.m
@@ -6,6 +6,21 @@ function rootDir = returnRootDir()
   %
   % (C) Copyright 2022 CPP_SPM developers
 
-  rootDir = fullfile(fileparts(mfilename('fullfile')), '..', '..');
-  rootDir =  spm_file(rootDir, 'cpath');
+  Mfile = 'bidsSpatialPrepro.m';
+  rootDir = cellstr(which(Mfile, '-ALL'));
+
+  if isempty(rootDir)
+    error('CPP_SPM is not in your MATLAB / Octave path.\n');
+
+  elseif numel(rootDir) > 1
+    fprintf('CPP_SPM seems to appear in several different folders:\n');
+    for i = 1:numel(rootDir)
+      fprintf('  * %s\n', fullfile(rootDir{i}, '..', '..'));
+    end
+    error('Remove all but one with ''pathtool''' .\ n'); % or ''spm_rmpath
+
+  end
+
+  rootDir = spm_file(fullfile(fileparts(rootDir{1}), '..', '..', '..'), 'cpath');
+
 end
