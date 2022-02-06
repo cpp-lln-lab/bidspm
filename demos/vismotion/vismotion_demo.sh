0e321a82af5d050e6fc77bc7b42f0018ba4278e0
Remi Gau <remi_gau@hotmail.com>
2022-02-06 18:09:38 +0100

    remove default stats model and update bash script

--- demos/vismotion/vismotion_demo.sh
+++ demos/vismotion/vismotion_demo.sh
@@ -1,42 +1,52 @@
 #! /user/bin/bash
 
+# IMPORTANT
+#
+# For this to work on your system you will need
+# to adapt this code to wherever MATLAB is on your computer (Look for the FIXME)
+#
+# What I used (see the line below) is very likely not where MATLAB lives on your computer
+# /usr/local/MATLAB/R2017a/bin/matlab
+
 # create dataset in home dir
-# datalad create -c yoda ~/visual_motion_localiser
+datalad create -c yoda ~/visual_motion_localiser
 cd ~/visual_motion_localiser
 
-# # get code  CPP SPM
-# datalad install \
-#     -d . \
-#     -s https://github.com/cpp-lln-lab/CPP_SPM.git \
-#     -r \
-#     code/CPP_SPM
+# get code  CPP SPM
+datalad install \
+    -d . \
+    -s https://github.com/cpp-lln-lab/CPP_SPM.git \
+    -r \
+    code/CPP_SPM
 
-# # change branch
-# cd code/CPP_SPM
-# git checkout dev
-# git submodule init
-# git submodule update
-# cd ../..
+# change branch
+cd code/CPP_SPM
+git checkout dev
+git submodule init
+git submodule update
+cd ../..
 
-# # get data
-# datalad install -d . \
-#     --get-data \
-#     -s git@gin.g-node.org:/cpp-lln-lab/CPP_visMotion-raw.git \
-#     inputs/raw
+# get data
+datalad install -d . \
+    --get-data \
+    -s git@gin.g-node.org:/cpp-lln-lab/CPP_visMotion-raw.git \
+    inputs/raw
 
-# datalad save -m 'set up done'
+datalad save -m 'set up done'
 
-# datlad create -d . outputs/derivatives/cpp_spm-preproc
-# datlad create -d . outputs/derivatives/cpp_spm-stats
+datlad create -d . outputs/derivatives/cpp_spm-preproc
+datlad create -d . outputs/derivatives/cpp_spm-stats
 
 cd code/CPP_SPM/demos/vismotion
 
+# FIX ME
 /usr/local/MATLAB/R2017a/bin/matlab \
     -nodisplay -nosplash -nodesktop \
     -r "run('step_1_preprocess.m');exit;"
 
 datalad save -m 'preprocessing done' -r
 
+# FIX ME
 /usr/local/MATLAB/R2017a/bin/matlab \
     -nodisplay -nosplash -nodesktop \
     -r "run('step_2_stats.m');exit;"
