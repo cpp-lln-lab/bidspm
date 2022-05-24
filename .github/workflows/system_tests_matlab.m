dd29b3842e4aa96369487346f8d9355fe6e467f7
Remi Gau <remi_gau@hotmail.com>
2022-05-24 18:23:37 +0200

    fix system test data download

--- .github/workflows/system_tests_matlab.m
+++ .github/workflows/system_tests_matlab.m
@@ -5,15 +5,15 @@ root_dir = getenv('GITHUB_WORKSPACE');
 
 addpath(fullfile(root_dir, 'spm12'));
 
+cd demos/MoAE;
+download_moae_ds(true);
+
 cd(fullfile(root_dir, 'manualTests'));
 
 run test_moae;
 
 % repeat this because test_moae has a "clear all"
 root_dir = getenv('GITHUB_WORKSPACE');
-
-cd demos/MoAE;
-download_moae_ds(true);
 
 cd(fullfile(root_dir, 'demos', 'face_repetition'));
 
