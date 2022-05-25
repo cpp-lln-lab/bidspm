f58ce66b05f46667e331534fcfddc8005307d742
Remi Gau <remi_gau@hotmail.com>
2022-05-25 19:26:04 +0200

    paralellize system tests

---
+++ .github/workflows/system_tests_facerep.m
@@ -0,0 +1,10 @@
+%
+% (C) Copyright 2021 CPP_SPM developers
+
+root_dir = getenv('GITHUB_WORKSPACE');
+
+addpath(fullfile(root_dir, 'spm12'));
+
+cd(fullfile(root_dir, 'demos', 'face_repetition'));
+
+run face_rep_resolution;
--- .github/workflows/system_tests_moae.m
+++ .github/workflows/system_tests_moae.m
@@ -11,13 +11,6 @@ initCppSpm;
 cd(fullfile(root_dir, 'demos', 'MoAE'));
 download_moae_ds(true);
 
-
 cd(fullfile(root_dir, 'manualTests'));
 run test_moae;
 
-% repeat this because test_moae has a "clear all"
-root_dir = getenv('GITHUB_WORKSPACE');
-
-cd(fullfile(root_dir, 'demos', 'face_repetition'));
-
-run face_rep_resolution;
