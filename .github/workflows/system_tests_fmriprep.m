fc7e16bb8ae31172479a58019981f718f69c2aaf
Remi Gau <remi_gau@hotmail.com>
2022-05-31 10:17:03 +0200

    no fmriprep grab from GIN

--- .github/workflows/system_tests_fmriprep.m
+++ .github/workflows/system_tests_fmriprep.m
@@ -5,6 +5,7 @@ root_dir = getenv('GITHUB_WORKSPACE');
 
 addpath(fullfile(root_dir, 'spm12'));
 
+% TODO replace with a data set from open neuro
 cd(fullfile(root_dir, 'demos', 'MoAE'));
 system('make inputs/fmriprep');
 run moae_fmriprep;
