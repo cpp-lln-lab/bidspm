b00d9083a05d34d228f4b361f5cc1b60e7a83fe0
Remi Gau <remi_gau@hotmail.com>
2022-05-31 21:34:43 +0200

    add test for renamePng

---
+++ tests/tests_unit/test_renamePng.m
@@ -0,0 +1,36 @@
+% (C) Copyright 2022 CPP_SPM developers
+
+function test_suite = test_renamePng %#ok<*STOUT>
+  try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+    test_functions = localfunctions(); %#ok<*NASGU>
+  catch % no problem; early Matlab versions can use initTestSuite fine
+  end
+  initTestSuite;
+end
+
+function test_renamePng_basic()
+
+  outputDir = fullfile(pwd, 'tmp');
+
+  if isdir(outputDir)
+    rmdir(outputDir, 's');
+  end
+
+  mkdir(outputDir);
+  system('touch tmp/sub-foo_designmatrix_001.png');
+  system('touch tmp/sub-foo_designmatrix_002.png');
+
+  files = dir(fullfile(outputDir, 'sub*_00*.png'));
+  assertEqual(numel(files), 2);
+
+  renamePng(outputDir);
+
+  files = dir(fullfile(outputDir, 'sub*_00*.png'));
+  assertEqual(numel(files), 0);
+
+  files = dir(fullfile(outputDir, 'sub*_designmatrix.png'));
+  assertEqual(numel(files), 1);
+
+  rmdir(outputDir, 's');
+
+end
