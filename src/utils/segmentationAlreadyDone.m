7d8498de657688d34c0ca933131193daba998358
Remi Gau <remi_gau@hotmail.com>
2022-06-12 22:48:04 +0200

    add functions to check for previous seg and strip outputs

---
+++ src/utils/segmentationAlreadyDone.m
@@ -0,0 +1,33 @@
+function status = segmentationAlreadyDone(anatFile, BIDS)
+  %
+  % USAGE::
+  %
+  %     status = checkForPreviousSegmentOutput(anatFile, BIDS, opt)
+  %
+  % :param anatFile:
+  % :type anatFile: path
+  %
+  % :returns: returns true
+  %
+  % to skip segmentation if done previously
+  %
+  %
+  %
+  % (C) Copyright 2022 CPP_SPM developers
+
+  anatFile = bids.File(anatFile);
+  filter = anatFile.entities;
+  filter.modality = 'anat';
+
+  filter.suffix = anatFile.suffix;
+  filter.desc = 'biascor';
+  filter.space = 'individual';
+  biasCorrectedImage = bids.query(BIDS, 'data', filter);
+
+  filter.suffix = 'probseg';
+  filter = rmfield(filter, 'desc');
+  tpm = bids.query(BIDS, 'data', filter);
+
+  status = numel(tpm) >= 3 && ~isempty(biasCorrectedImage);
+
+end
