8a4713db14b58bb62edecf78127194af3fb44745
Remi Gau <remi_gau@hotmail.com>
2022-06-12 23:07:51 +0200

    refactor

--- src/batches/preproc/setBatchSegmentation.m
+++ src/batches/preproc/setBatchSegmentation.m
@@ -30,16 +30,15 @@ function matlabbatch = setBatchSegmentation(matlabbatch, opt, imageToSegment)
 
   % save bias correction field = false
   % save bias corrected image = true
-  matlabbatch{end + 1}.spm.spatial.preproc.channel.write = [false true];
+  preproc.channel.write = [false true];
 
   % firts part assumes we are in the bidsSpatialPreproc workflow
   if isfield(opt, 'orderBatches') && isfield(opt.orderBatches, 'selectAnat')
 
     % SAVE BIAS CORRECTED IMAGE
-    matlabbatch{end}.spm.spatial.preproc.channel.vols(1) = ...
-        cfg_dep('Named File Selector: Anatomical(1) - Files', ...
-                returnDependency(opt, 'selectAnat'), ...
-                substruct('.', 'files', '{}', {1}));
+    preproc.channel.vols(1) = cfg_dep('Named File Selector: Anatomical(1) - Files', ...
+                                      returnDependency(opt, 'selectAnat'), ...
+                                      substruct('.', 'files', '{}', {1}));
   else
 
     % TODO
@@ -54,60 +53,56 @@ function matlabbatch = setBatchSegmentation(matlabbatch, opt, imageToSegment)
     % add all the images to segment
     for iImg = 1:size(imageToSegment, 1)
       file = validationInputFile([], deblank(imageToSegment(iImg, :)));
-      matlabbatch{end}.spm.spatial.preproc.channel.vols{iImg, 1} = file;
+      preproc.channel.vols{iImg, 1} = file;
     end
 
   end
 
-  matlabbatch{end}.spm.spatial.preproc.channel.biasreg = 0.001;
-  matlabbatch{end}.spm.spatial.preproc.channel.biasfwhm = opt.segment.biasfwhm;
+  preproc.channel.biasreg = 0.001;
+  preproc.channel.biasfwhm = opt.segment.biasfwhm;
 
   % CREATE SEGMENTS IN NATIVE SPACE OF GM, WM AND CSF
   nativeSpace = 1;
   dartelSpace = 0;
   native = [nativeSpace dartelSpace];
-  matlabbatch{end}.spm.spatial.preproc.tissue(1).tpm = ...
-      {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',1']};
-  matlabbatch{end}.spm.spatial.preproc.tissue(1).ngaus = 1;
-  matlabbatch{end}.spm.spatial.preproc.tissue(1).native = native;
-  matlabbatch{end}.spm.spatial.preproc.tissue(1).warped = [0 0];
+  preproc.tissue(1).tpm = {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',1']};
+  preproc.tissue(1).ngaus = 1;
+  preproc.tissue(1).native = native;
+  preproc.tissue(1).warped = [0 0];
 
-  matlabbatch{end}.spm.spatial.preproc.tissue(2).tpm = ...
-      {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',2']};
-  matlabbatch{end}.spm.spatial.preproc.tissue(2).ngaus = 1;
-  matlabbatch{end}.spm.spatial.preproc.tissue(2).native = native;
-  matlabbatch{end}.spm.spatial.preproc.tissue(2).warped = [0 0];
+  preproc.tissue(2).tpm = {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',2']};
+  preproc.tissue(2).ngaus = 1;
+  preproc.tissue(2).native = native;
+  preproc.tissue(2).warped = [0 0];
 
-  matlabbatch{end}.spm.spatial.preproc.tissue(3).tpm = ...
-      {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',3']};
-  matlabbatch{end}.spm.spatial.preproc.tissue(3).ngaus = 2;
-  matlabbatch{end}.spm.spatial.preproc.tissue(3).native = native;
-  matlabbatch{end}.spm.spatial.preproc.tissue(3).warped = [0 0];
+  preproc.tissue(3).tpm = {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',3']};
+  preproc.tissue(3).ngaus = 2;
+  preproc.tissue(3).native = native;
+  preproc.tissue(3).warped = [0 0];
 
-  matlabbatch{end}.spm.spatial.preproc.tissue(4).tpm = ...
-      {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',4']};
-  matlabbatch{end}.spm.spatial.preproc.tissue(4).ngaus = 3;
-  matlabbatch{end}.spm.spatial.preproc.tissue(4).native = [0 0];
-  matlabbatch{end}.spm.spatial.preproc.tissue(4).warped = [0 0];
+  preproc.tissue(4).tpm = {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',4']};
+  preproc.tissue(4).ngaus = 3;
+  preproc.tissue(4).native = [0 0];
+  preproc.tissue(4).warped = [0 0];
 
-  matlabbatch{end}.spm.spatial.preproc.tissue(5).tpm = ...
-      {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',5']};
-  matlabbatch{end}.spm.spatial.preproc.tissue(5).ngaus = 4;
-  matlabbatch{end}.spm.spatial.preproc.tissue(5).native = [0 0];
-  matlabbatch{end}.spm.spatial.preproc.tissue(5).warped = [0 0];
+  preproc.tissue(5).tpm = {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',5']};
+  preproc.tissue(5).ngaus = 4;
+  preproc.tissue(5).native = [0 0];
+  preproc.tissue(5).warped = [0 0];
 
-  matlabbatch{end}.spm.spatial.preproc.tissue(6).tpm = ...
-      {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',6']};
-  matlabbatch{end}.spm.spatial.preproc.tissue(6).ngaus = 2;
-  matlabbatch{end}.spm.spatial.preproc.tissue(6).native = [0 0];
-  matlabbatch{end}.spm.spatial.preproc.tissue(6).warped = [0 0];
+  preproc.tissue(6).tpm = {[fullfile(spmLocation, 'tpm', 'TPM.nii') ',6']};
+  preproc.tissue(6).ngaus = 2;
+  preproc.tissue(6).native = [0 0];
+  preproc.tissue(6).warped = [0 0];
 
   % SAVE FORWARD DEFORMATION FIELD FOR NORMALISATION
-  matlabbatch{end}.spm.spatial.preproc.warp.mrf = 1;
-  matlabbatch{end}.spm.spatial.preproc.warp.cleanup = 1;
-  matlabbatch{end}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
-  matlabbatch{end}.spm.spatial.preproc.warp.affreg = 'mni';
-  matlabbatch{end}.spm.spatial.preproc.warp.fwhm = 0;
-  matlabbatch{end}.spm.spatial.preproc.warp.samp = opt.segment.samplingDistance;
-  matlabbatch{end}.spm.spatial.preproc.warp.write = [1 1];
+  preproc.warp.mrf = 1;
+  preproc.warp.cleanup = 1;
+  preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+  preproc.warp.affreg = 'mni';
+  preproc.warp.fwhm = 0;
+  preproc.warp.samp = opt.segment.samplingDistance;
+  preproc.warp.write = [1 1];
+
+  matlabbatch{end + 1}.spm.spatial.preproc = preproc;
 end
