72ca910bdb033a1064b767b9f5600d683a1dcbe1
Remi Gau <remi_gau@hotmail.com>
2022-06-27 19:39:24 +0200

    FCI

--- .github/workflows/system_tests_fmriprep.m
+++ .github/workflows/system_tests_fmriprep.m
@@ -7,6 +7,6 @@ fprintf('\nroot dir is %s\n', root_dir);
 
 addpath(fullfile(root_dir, 'spm12'));
 
-% TODO replace with a data set from open neuro
 cd(fullfile(root_dir, 'demos', 'MoAE'));
+
 run moae_fmriprep;
--- .github/workflows/tests_octave.m
+++ .github/workflows/tests_octave.m
@@ -1,13 +1,49 @@
-%
-% (C) Copyright 2021 CPP_SPM developers
+function tests_octave()
+  %
+  % (C) Copyright 2021 CPP_SPM developers
 
-root_dir = getenv('GITHUB_WORKSPACE');
+  % Elapsed time is 284 seconds.
 
-cd(root_dir);
-cpp_spm('action', 'dev');
+  more off;
 
-cd demos/MoAE;
-download_moae_ds(true);
+  tic;
 
-cd(root_dir);
-run run_tests();
+  cd(fullfile(fileparts(mfilename('fullpath')), '..', '..'));
+
+  addpath(fullfile(pwd, 'tests', 'utils'));
+
+  cpp_spm();
+
+  if isGithubCi
+    printToScreen('\nThis is github CI\n');
+  else
+    printToScreen('\nThis is not github CI\n');
+  end
+
+  printToScreen(sprintf('\nHome is %s\n', getenv('HOME')));
+
+  warning('OFF');
+
+  spm('defaults', 'fMRI');
+
+  subfolder = '';
+
+  folderToCover = fullfile(pwd, 'src');
+  testFolder = fullfile(pwd, 'tests', subfolder);
+
+  success = moxunit_runtests( ...
+                             testFolder, ...
+                             '-verbose', '-recursive', '-with_coverage', ...
+                             '-cover', folderToCover, ...
+                             '-cover_xml_file', 'coverage.xml', ...
+                             '-cover_html_dir', fullfile(pwd, 'coverage_html'));
+
+  if success
+    system('echo 0 > test_report.log');
+  else
+    system('echo 1 > test_report.log');
+  end
+
+  toc;
+
+end
