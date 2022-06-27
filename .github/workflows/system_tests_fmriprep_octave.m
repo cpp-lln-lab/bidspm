a81ea88ed9d53c004eef24bee9c096471229c988
Remi Gau <remi_gau@hotmail.com>
2022-06-27 16:54:02 +0200

    FCI

---
+++ .github/workflows/system_tests_fmriprep_octave.m
@@ -0,0 +1,10 @@
+%
+% (C) Copyright 2021 CPP_SPM developers
+
+root_dir = getenv('GITHUB_WORKSPACE');
+
+addpath(fullfile(root_dir, 'spm12'));
+
+% TODO replace with a data set from open neuro
+cd(fullfile(root_dir, 'demos', 'MoAE'));
+run moae_fmriprep;
