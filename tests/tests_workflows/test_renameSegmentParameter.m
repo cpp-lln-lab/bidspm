f00b36cf35ac925d98d55a89beebe59bd143d82e
Remi Gau <remi_gau@hotmail.com>
2022-07-20 10:45:14 +0200

    refactor renaming functions

---
+++ tests/tests_workflows/test_renameSegmentParameter.m
@@ -0,0 +1,42 @@
+function test_suite = test_renameSegmentParameter %#ok<*STOUT>
+  %
+  % (C) Copyright 2022 CPP_SPM developers
+
+  try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+    test_functions = localfunctions(); %#ok<*NASGU>
+  catch % no problem; early Matlab versions can use initTestSuite fine
+  end
+  initTestSuite;
+end
+
+function test_renameSegmentParameter_basic()
+
+  opt = setOptions('MoAE-preproc');
+
+  % move test data into temp directory to test renaming
+  tmpDir = fullfile(pwd, 'tmp');
+  if isdir(tmpDir)
+    rmdir(tmpDir, 's');
+  end
+  spm_mkdir(tmpDir);
+  copyfile(opt.dir.preproc, tmpDir);
+
+  BIDS = bids.layout(tmpDir, 'use_schema', false);
+
+  opt.dryRun = false;
+  opt.verbosity = 2;
+
+  segParamFiles = spm_select('FPListRec', tmpDir, '^.*_T1w_seg8.mat$');
+  assertEqual(size(segParamFiles, 1), 1);
+
+  renameSegmentParameter(BIDS, '01', opt);
+
+  segParamFiles = spm_select('FPListRec', tmpDir, '^.*_T1w_seg8.mat$');
+  assertEqual(size(segParamFiles, 1), 0);
+
+  segParamFiles = spm_select('FPListRec', tmpDir, '^.*label-T1w_segparam.mat$');
+  assertEqual(size(segParamFiles, 1), 1);
+
+  rmdir(tmpDir, 's');
+
+end
---
+++ tests/tests_workflows/test_renameUnwarpParameter.m
@@ -0,0 +1,42 @@
+function test_suite = test_renameUnwarpParameter %#ok<*STOUT>
+  %
+  % (C) Copyright 2022 CPP_SPM developers
+
+  try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+    test_functions = localfunctions(); %#ok<*NASGU>
+  catch % no problem; early Matlab versions can use initTestSuite fine
+  end
+  initTestSuite;
+end
+
+function test_renameUnwarpParameter_basic()
+
+  opt = setOptions('MoAE-preproc');
+
+  % move test data into temp directory to test renaming
+  tmpDir = fullfile(pwd, 'tmp');
+  if isdir(tmpDir)
+    rmdir(tmpDir, 's');
+  end
+  spm_mkdir(tmpDir);
+  copyfile(opt.dir.preproc, tmpDir);
+
+  BIDS = bids.layout(tmpDir, 'use_schema', false);
+
+  opt.dryRun = false;
+  opt.verbosity = 2;
+
+  unwarpParamFiles = spm_select('FPListRec', tmpDir, '^.*_bold_uw.mat$');
+  assertEqual(size(unwarpParamFiles, 1), 1);
+
+  renameUnwarpParameter(BIDS, '01', opt);
+
+  unwarpParamFiles = spm_select('FPListRec', tmpDir, '^.*_bold_uw.mat$');
+  assertEqual(size(unwarpParamFiles, 1), 0);
+
+  unwarpParamFiles = spm_select('FPListRec', tmpDir, '^.*label-bold_unwarpparam.mat$');
+  assertEqual(size(unwarpParamFiles, 1), 1);
+
+  rmdir(tmpDir, 's');
+
+end
