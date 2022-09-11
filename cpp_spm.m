2396e3507bdac747c2ff3eb18e9575f575a9a25f
Remi Gau <remi_gau@hotmail.com>
2022-09-11 12:34:53 +0200

    fix typo

--- cpp_spm.m
+++ cpp_spm.m
@@ -2,7 +2,7 @@ function cpp_spm(varargin)
   % (C) Copyright 2021 bidspm developers
 
   warning(['"cpp_spm" will be deprecated in the next major release.\n', ...
-           Please use the new "bidspm" interface instead.']);
+           'Please use the new "bidspm" interface instead.']);
 
   bidsspm(varargin);
 
