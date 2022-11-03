95354be2a50add5a3977e43ec55e705ed126470f
Remi Gau <remi_gau@hotmail.com>
2022-11-03 12:34:41 +0100

    refactor validation

---
+++ src/validate.py
@@ -0,0 +1,78 @@
+import argparse
+import logging
+import sys
+from pathlib import Path
+from typing import Any
+
+import pydantic
+from bsmschema.models import BIDSStatsModel
+from rich.logging import RichHandler
+
+log = logging.getLogger("bidspm")
+
+
+def bidspm_log(name: str = "bidspm") -> logging.Logger:
+    """Create log."""
+
+    FORMAT = "bidspm - %(asctime)s - %(levelname)s - %(message)s"
+
+    if not name:
+        name = "rich"
+
+    logging.basicConfig(
+        level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+    )
+
+    return logging.getLogger(name)
+
+
+def validate_parser():
+
+    log = bidspm_log(name="bidspm")
+
+    parser = argparse.ArgumentParser(
+        description="validate bids stats model",
+    )
+
+    parser.add_argument(
+        "model",
+        help="""
+        The bids stats model file. If a directory is provided,
+        all files ending in '_smdl.json' will be validated.
+        """,
+        nargs=1,
+    )
+
+    return parser
+
+
+def validate(file: Path) -> None:
+    file = file.absolute()
+    log.info(f"Validating {file}")
+    try:
+        BIDSStatsModel.parse_file(file)
+    except pydantic.error_wrappers.ValidationError as e:
+        log.warning(f"{file} is not a valid BIDS Stats Model file.")
+        log.warning(e)
+
+
+def main(argv: Any = sys.argv) -> None:
+
+    parser = validate_parser()
+    args = parser.parse_args(argv[1:])
+
+    input_ = Path(args.model[0])
+
+    if not input_.exists():
+        raise FileNotFoundError(f"{input_} does not exist.")
+
+    elif input_.is_file():
+        validate(input_)
+
+    if input_.is_dir():
+        for file in input_.glob("*_smdl.json"):
+            validate(file)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/tests_bids/test_validate.m
@@ -0,0 +1,50 @@
+% (C) Copyright 2022 bidspm developers
+
+function test_suite = test_validate %#ok<*STOUT>
+  try % assignment of 'localfunctions' is necessary in Matlab >= 2016
+    test_functions = localfunctions(); %#ok<*NASGU>
+  catch % no problem; early Matlab versions can use initTestSuite fine
+  end
+  initTestSuite;
+end
+
+function test_validate_skip()
+
+  args.Results.skip_validation = true;
+  validate(args);
+end
+
+function test_validate_dataset()
+
+  args.Results.skip_validation = false;
+  args.Results.bids_dir = pwd;
+  assertExceptionThrown(@() validate(args), 'validate:invalidBidsDataset');
+
+end
+
+function test_validate_quiet()
+
+  opt = setOptions('MoAE');
+
+  args.Results.skip_validation = false;
+  args.Results.action = 'preproc';
+  args.Results.bids_dir = opt.dir.raw;
+  args.Results.versbosity = 0;
+
+  validate(args);
+
+end
+
+function test_validate_model()
+
+  opt = setOptions('MoAE');
+
+  args.Results.skip_validation = false;
+  args.Results.action = 'stats';
+  args.Results.bids_dir = opt.dir.raw;
+  args.Results.model_file = fullfile(opt.dir.raw, '..', '..', 'models');
+  args.Results.versbosity = 3;
+
+  assertWarning(@() validate(args), 'validate:invalidModel');
+
+end
