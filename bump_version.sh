5f1939617acb536e3cb4bf60ec83db3699d2bcfa
Remi Gau <remi_gau@hotmail.com>
2022-11-03 09:51:25 +0100

    improve docekr file building

--- bump_version.sh
+++ bump_version.sh
@@ -5,6 +5,8 @@
 grep -w "^version" CITATION.cff | sed "s/version: /v/g" >version.txt
 VERSION=$(cat version.txt | cut -c2-)
 sed -i "s/version=.*/version=\"$VERSION\"/g" Dockerfile
+sed -i "s/--branch .* --depth 1/--branch v$VERSION --depth 1/g" Dockerfile
+
 sed -i "s/version=.*/version=\"$VERSION\"/g" Dockerfile_matlab
 sed -i "s/version = {.*/version = {$VERSION}/g" README.md
 sed -i "s/__version__ = .*/version = {$VERSION}/g" README.md
