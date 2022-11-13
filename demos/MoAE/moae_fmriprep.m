4ad09fb5f7f50165b0780abf30ee689632b4896b
Remi Gau <remi_gau@hotmail.com>
2022-11-13 11:19:06 +0100

    update default mode filename for MoAE fmriprep demo

--- demos/MoAE/moae_fmriprep.m
+++ demos/MoAE/moae_fmriprep.m
@@ -95,7 +95,7 @@ bidspm(bids_dir, output_dir, 'dataset', ...
 
 % Run model
 
-model_file = fullfile(output_dir, 'models', 'model-MoAEfmriprep_smdl.json');
+model_file = fullfile(output_dir, 'models', 'model-default_smdl.json');
 
 subject_label = '01';
 
