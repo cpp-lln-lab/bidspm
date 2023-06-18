7d5297932d1c9c3d86e07ea3ab7e743bfff6dd9c
Remi Gau <remi_gau@hotmail.com>
2023-06-19 00:42:10 +0200

    [INFRA] update submodules and split windows testing (#1048)
    * update submodules
    
    * split test windows and other OSes
    
    * update submodules when updating bidspm
    
    * do not use sequence
    
    * update bids matlab

--- tools/update.sh
+++ tools/update.sh
@@ -6,3 +6,4 @@ git remote add tmp https://github.com/cpp-lln-lab/bidspm.git
 git fetch tmp
 git pull tmp ${branch}
 git remote remove tmp
+git submodule update --init --recursive && git submodule update --recursive
