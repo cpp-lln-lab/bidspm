2266c5107b2af1738ab17709ed4af356d27a6401
Remi Gau <remi_gau@hotmail.com>
2023-07-30 17:52:59 -0400

    [ENH] support label of activations with all atlases (#1100)
    * support label of activations with all atlases
    
    * update changelog
    
    * update doc
    
    * update doc

--- src/workflows/stats/bidsResults.m
+++ src/workflows/stats/bidsResults.m
@@ -81,8 +81,13 @@ function matlabbatch = bidsResults(varargin)
   % will be labelled.
   % You can specify the atlas to use for that by choosing between
   %
-  %  - `'Neuromorphometrics'` (default)
-  %  - `'aal'`
+  %  - ``'Neuromorphometrics'`` (default)
+  %  - ``'aal'``
+  %  - ``'visfatlas'``
+  %  - ``'anatomy_toobox'``
+  %  - ``'hcpex'``
+  %  - ``'glasser'``
+  %  - ``'wang'``
   %
   % .. code-block:: matlab
   %
@@ -131,10 +136,6 @@ function matlabbatch = bidsResults(varargin)
   %
 
   % (C) Copyright 2020 bidspm developers
-
-  % TODO move ps / png file: difficult at subject level because there can be
-  %                          several png for one contrast
-  % TODO rename NIDM file:
 
   matlabbatch = {};
 
