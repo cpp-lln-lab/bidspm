6649cf0a1d1913495fcdd1230c3fc2193dc0a46a
Remi Gau <remi_gau@hotmail.com>
2023-08-09 22:19:57 -0400

    [DATALAD] Recorded changes

--- src/cli/inputParserForSmooth.m
+++ src/cli/inputParserForSmooth.m
@@ -9,9 +9,7 @@ function args = inputParserForSmooth()
 
   isPositiveScalar = @(x) isnumeric(x) && numel(x) == 1 && x >= 0;
   isLogical = @(x) islogical(x) && numel(x) == 1;
-  isCellStr = @(x) iscellstr(x);
 
-  addParameter(args, 'space', {}, isCellStr);
   addParameter(args, 'fwhm', 6, isPositiveScalar);
   addParameter(args, 'dry_run', false, isLogical);
 end
