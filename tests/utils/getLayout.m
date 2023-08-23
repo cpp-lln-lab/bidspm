85a8766c17f26614b063b3c6564260bbceba85e2
Remi Gau <remi_gau@hotmail.com>
2023-08-23 19:15:39 -0400

    generate layout mat before running tests

--- tests/utils/getLayout.m
+++ tests/utils/getLayout.m
@@ -5,9 +5,13 @@ function BIDS = getLayout(opt)
 
   % (C) Copyright 2022 bidspm developers
   try
+    fprintf('\n\n LOADING: %s \n_n', fullfile(opt.dir.preproc, 'layout.mat'));
     load(fullfile(opt.dir.preproc, 'layout.mat'), 'BIDS');
   catch
-    BIDS = bids.layout(opt.dir.preproc, 'use_schema', false, 'verbose', opt.verbosity > 0, ...
+    fprintf('\n\n INDEXING: %s \n_n', opt.dir.preproc);
+    BIDS = bids.layout(opt.dir.preproc, ...
+                       'use_schema', false, ...
+                       'verbose', opt.verbosity > 0, ...
                        'index_dependencies', false);
     BIDS.raw = bids.layout(opt.dir.raw);
   end
