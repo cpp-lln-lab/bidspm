03f8dbc118d5f95849e20d72aef574520200832e
Remi Gau <remi_gau@hotmail.com>
2023-08-26 17:27:26 -0400

    update doc

--- lib/utils/range.m
+++ lib/utils/range.m
@@ -1,17 +1,18 @@
 function y = range(x,dim)
-%RANGE  Sample range.
-%   Y = RANGE(X) returns the range of the values in X.  For a vector input,
-%   Y is the difference between the maximum and minimum values.  For a
-%   matrix input, Y is a vector containing the range for each column.  For
-%   N-D arrays, RANGE operates along the first non-singleton dimension.
+% Sample range.
 %
-%   RANGE treats NaNs as missing values, and ignores them.
+% USAGE::
 %
-%   Y = RANGE(X,DIM) operates along the dimension DIM.
+%   Y = range(X) returns the range of the values in X.
 %
-%   See also BOUNDS, MIN, MAX, IQR, MAD, STD.
-
-%   Copyright 1993-2016 The MathWorks, Inc.
+%   For a vector input, Y is the difference between the maximum and minimum values
+%   For a matrix input, Y is a vector containing the range for each column.
+%   For N-D arrays, range operates along the first non-singleton dimension.
+%
+%   range treats NaNs as missing values, and ignores them.
+%
+%   Y = range(X,DIM) operates along the dimension DIM.
+%
 
 
 if nargin < 2
