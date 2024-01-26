35000197cc5effe62c7ea3462012a03d9ae1a284
Remi Gau <remi_gau@hotmail.com>
2024-01-26 18:19:18 +0100

    add stats command

--- demos/openneuro/ds000001_run.sh
+++ demos/openneuro/ds000001_run.sh
@@ -3,8 +3,17 @@
 # script to run the demo via the command line
 cd ../..
 
+# bidspm demos/openneuro/inputs/ds000001 demos/openneuro/outputs/ds000001/derivatives subject \
+#     --action preprocess \
+#     --task balloonanalogrisktask \
+#     --fwhm 8 \
+#     --participant_label 01 02
+
+
 bidspm demos/openneuro/inputs/ds000001 demos/openneuro/outputs/ds000001/derivatives subject \
-    --action preprocess \
+    --action stats \
+    --preproc_dir demos/openneuro/outputs/ds000001/derivatives/bidspm-preproc \
     --task balloonanalogrisktask \
+    --model_file demos/openneuro/models/model-balloonanalogrisktaskDefault_smdl.json \
     --fwhm 8 \
-    --participant_label 01 02 03
+    --participant_label 01 02
