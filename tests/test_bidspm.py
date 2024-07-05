d65cfbfdfe8e8291c0796a44b1fb61b9eff8b83f
Remi Gau <remi_gau@hotmail.com>
2024-07-05 09:35:22 +0200

    fix test

--- tests/test_bidspm.py
+++ tests/test_bidspm.py
@@ -24,7 +24,7 @@ def test_base_cmd():
     bids_dir = Path("/path/to/bids")
     output_dir = Path("/path/to/output")
     cmd = base_cmd(bids_dir, output_dir)
-    assert cmd == " bidspm(); bidspm('/path/to/bids', ...\n\t\t\t '/path/to/output'"
+    assert cmd == " bidspm('init'); bidspm( '/path/to/bids', ...\n\t '/path/to/output'"
 
 
 def test_parser():
@@ -58,7 +58,7 @@ def test_append_common_arguments():
     )
     assert (
         cmd
-        == ", ...\n\t\t\t 'fwhm', 6, ...\n\t\t\t 'participant_label', { '01', '02' }, ...\n\t\t\t 'skip_validation', true, ...\n\t\t\t 'dry_run', true"
+        == ", ...\n\t 'fwhm', 6, ...\n\t 'participant_label', { '01', '02' }, ...\n\t 'skip_validation', true, ...\n\t 'dry_run', true"
     )
 
 
@@ -68,7 +68,7 @@ def test_append_base_arguments():
     )
     assert (
         cmd
-        == ", ...\n\t\t\t 'verbosity', 0, ...\n\t\t\t 'space', { 'foo', 'bar' }, ...\n\t\t\t 'task', { 'spam', 'eggs' }, ...\n\t\t\t 'ignore', { 'nii' }"
+        == ", ...\n\t 'verbosity', 0, ...\n\t 'space', { 'foo', 'bar' }, ...\n\t 'task', { 'spam', 'eggs' }, ...\n\t 'ignore', { 'nii' }"
     )
 
 
