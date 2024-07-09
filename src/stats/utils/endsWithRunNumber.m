33a60537c9be18effa08e3595b852ba27560f275
Remi Gau <remi_gau@hotmail.com>
2024-07-09 17:03:06 +0200

    update regex

--- src/stats/utils/endsWithRunNumber.m
+++ src/stats/utils/endsWithRunNumber.m
@@ -5,10 +5,14 @@ function value = endsWithRunNumber(contrastName)
   %   endsWithRunNumber(contrastName)
   %
   %
-  % Returns true if the contrast name is that for a run.
+  % Returns true if the contrast name ends with an underscore
+  % followed by some number.
   %
 
   % (C) Copyright 2024 bidspm developers
-  result = regexp(contrastName, '_run-[0-9]*$', 'match');
+  result = regexp( ...
+                  contrastName, ...
+                  '.*(_[0-9]+\$?$|_(?:ses|run)-[0-9]*$|_ses-[0-9]*_run-[0-9]*$)', ...
+                  'match');
   value =  ~isempty(result);
   return
