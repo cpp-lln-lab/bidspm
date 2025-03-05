1ff190fafaae29635fed4583cf34c6ba853c1675
Marco Barilari <38101692+marcobarilari@users.noreply.github.com>
2025-03-05 16:23:10 +0100

    Add defaults for transparent plots (#1355)
    * add local path
    
    * add local path
    
    * add chagelog CI script
    
    * add code
    
    * add mock change
    
    * update changelog workflow
    
    * fix typo
    
    * fix syntax
    
    * fix CI error
    
    * saved UNDO information for reset changes and discarded files
    
    * update bids-matlab submodule to latest commit in main
    
    * update gitmodule file to track main branch in bids-matlab
    
    * rm files we do not know where they are coming from
    
    * add defaults for transparent plots
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update src/defaults/defaultResultsStructure.m
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update src/defaults/defaultResultsStructure.m
    
    * assign the sd structures to the result one
    
    * fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add test and update
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Remi Gau <remi_gau@hotmail.com>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- src/defaults/defaultResultsStructure.m
+++ src/defaults/defaultResultsStructure.m
@@ -23,4 +23,35 @@ function result  = defaultResultsStructure()
 
   result.nidm = true();
 
+  % transparent plot
+  layers{1} = struct('color', struct('file', result.montage.background, ...
+                                     'range', [0 1], ...
+                                     'map', gray(256)));
+
+  %   color_map_folder = fullfile(fileparts(which('map_luminance')), '..', 'mat_maps');
+  %   load(fullfile(color_map_folder, 'diverging_bwr_iso.mat'));
+
+  layers{2} = struct('color', struct('file', [], ... % con image
+                                     'map', 'diverging_bwr_iso', ...
+                                     'range', [-4 4]), ...
+                     'label', '\beta_{listening} - \beta_{baseline} (a.u.)', ...
+                     'opacity', struct('file', [], ... % spmT image
+                                       'range', [2 3], ...
+                                       'label', '| t |'));
+
+  layers{3} = struct('color', struct('file', [], ... % spmT mask thresholded at 0.05 FWD
+                                     'map', [0 0 0], ...
+                                     'line_width', 2));
+
+  result.sdConfig.layers = layers;
+
+  settings = sd_config_settings('init');
+
+  settings.slice.orientation = result.montage.orientation;
+  settings.slice.disp_slices = -50:10:50;
+  settings.fig_specs.n.slice_column = 4;
+  settings.fig_specs.title = [];
+
+  result.sdConfig.settings = settings;
+
 end
