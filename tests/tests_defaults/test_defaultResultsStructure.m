c6ad77f9683c42dc6eddbcf23ad8c840c573e5ba
Remi Gau <remi_gau@hotmail.com>
2025-03-05 15:35:28 +0100

    add test and update

--- tests/tests_defaults/test_defaultResultsStructure.m
+++ tests/tests_defaults/test_defaultResultsStructure.m
@@ -36,6 +36,36 @@ function test_defaultResultsStructure_basic()
   expected.montage.background = fullfile(spm('dir'), 'canonical', 'avg152T1.nii');
 
   expected.nidm = true();
+  
+  
+  layers{1} = struct('color', struct('file', expected.montage.background, ...
+                                     'range', [0 1], ...
+                                     'map', gray(256)));
+
+  layers{2} = struct('color', struct('file', [], ... % con image
+                                     'map', 'diverging_bwr_iso', ...
+                                     'range', [-4 4]), ...
+                     'label', '\beta_{listening} - \beta_{baseline} (a.u.)', ...
+                     'opacity', struct('file', [], ... % spmT image
+                                       'range', [2 3], ...
+                                       'label', '| t |'));
+
+  layers{3} = struct('color', struct('file', [], ... % spmT mask thresholded at 0.05 FWD
+                                     'map', [0 0 0], ...
+                                     'line_width', 2));
+
+  expected.sdConfig.layers = layers;
+
+  settings = sd_config_settings('init');
+  
+  settings = sd_config_settings('init');
+
+  settings.slice.orientation = expected.montage.orientation;
+  settings.slice.disp_slices = -50:10:50;
+  settings.fig_specs.n.slice_column = 4;
+  settings.fig_specs.title = [];
+
+  expected.sdConfig.settings = settings;
 
   assertEqual(results, expected);
 
